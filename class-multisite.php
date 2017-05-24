5778d81b6dd90576a453c70066a7fb2c6ef1bfcc
Rogier Lankhorst <rogierlankhorst@gmail.com>
2017-05-24 09:08:44 +0200

    multisite dismiss message not dismissing

--- class-multisite.php
+++ class-multisite.php
@@ -40,10 +40,12 @@ if ( ! class_exists( 'rsssl_multisite' ) ) {
 
     add_action('network_admin_menu', array( &$this, 'add_multisite_menu' ) );
     add_action('network_admin_edit_rsssl_update_network_settings',  array($this,'update_network_options'));
-    add_action('network_admin_notices', array($this, 'show_notices'), 10);
-    add_action('admin_print_footer_scripts', array($this, 'insert_dismiss_success'));
+    if (is_network_admin()) {
+      add_action('network_admin_notices', array($this, 'show_notices'), 10);
+      add_action('admin_print_footer_scripts', array($this, 'insert_dismiss_success'));
+    }
 
-    add_action('wp_ajax_dismiss_success_message', array($this,'dismiss_success_message_callback') );
+    add_action('wp_ajax_dismiss_success_message_multisite', array($this,'dismiss_success_message_callback') );
 
     add_action('wp_ajax_rsssl_pro_dismiss_pro_option_notice', array($this,'dismiss_pro_option_notice') );
     add_action("network_admin_notices", array($this, 'show_pro_option_notice'));
@@ -627,7 +629,7 @@ $ajax_nonce = wp_create_nonce( "really-simple-ssl-dismiss" );
   jQuery(document).ready(function($) {
     $(".rlrsssl-success.notice.is-dismissible").on("click", ".notice-dismiss", function(event){
           var data = {
-            'action': 'dismiss_success_message',
+            'action': 'dismiss_success_message_multisite',
             'security': '<?php echo $ajax_nonce; ?>'
           };
 
