28cf7a6b7e49b81e132db7b7e36e756927cebae9
Rogier Lankhorst <rogierlankhorst@gmail.com>
2018-07-09 18:06:02 +0200

    check for http host

--- class-front-end.php
+++ class-front-end.php
@@ -79,6 +79,8 @@ if ( ! class_exists( 'rsssl_front_end' ) ) {
             //check for Command Line
             if (php_sapi_name() === 'cli') return;
 
+            if (!array_key_exists('HTTP_HOST', $_SERVER)) return;
+
             if ($this->ssl_enabled && !is_ssl() && !(defined("rsssl_no_rest_api_redirect") && rsssl_no_rest_api_redirect)) {
                 $redirect_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
                 wp_redirect($redirect_url, 301);
