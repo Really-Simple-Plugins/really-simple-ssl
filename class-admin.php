f4aee1cbf93d9669fd9d5bbc41db88d07bd63be7
mwolters90 <mark@really-simple-ssl.com>
2019-07-05 11:22:29 +0200

    removed update_options

--- class-admin.php
+++ class-admin.php
@@ -48,15 +48,7 @@ class rsssl_admin extends rsssl_front_end
 
     function __construct()
     {
-
-	    update_option('rsssl_mixed_content_fixer_detected_dismissed', false);
-	    update_option('rsssl_check_redirect_dismissed', false);
-	    update_option('rsssl_ssl_detected_dismissed', false);
-	    update_option('rsssl_check_redirect_dismissed', false);
-	    update_option('rsssl_hsts_enabled_dismissed', false);
-	    update_option('rsssl_secure_cookies_set_dismissed', false);
-	    update_option('rsssl_mixed_content_scan_dismissed', false);
-
+        
         if (isset(self::$_this))
             wp_die(sprintf(__('%s is a singleton class and you cannot create a second instance.', 'really-simple-ssl'), get_class($this)));
 
