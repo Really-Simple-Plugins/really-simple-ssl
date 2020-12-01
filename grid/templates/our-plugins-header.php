fa64e5f5191ec297a1cbba716be7abb061ff424c
Mark-met-een-K <markie_wolters77@hotmail.com>
2020-12-01 11:40:33 +0100

    RSP logo in our plugins

---
+++ grid/templates/our-plugins-header.php
@@ -0,0 +1,7 @@
+<div class="rsssl-secondary-header-item">
+    <div id="rsssl-rsp-image">
+        <a href="https://really-simple-plugins.com/">
+            <img src="<?php echo rsssl_url?>/assets/really-simple-plugins.png" alt="Really Simple Plugins Logo">
+        </a>
+    </div>
+</div>
