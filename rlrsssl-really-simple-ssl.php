170df30f43e3faa6a75a4e6c7ea3a3141b7dea2f
Rogier Lankhorst <rogier@really-simple-plugins.com>
2020-12-10 14:07:25 +0100

    readme

--- rlrsssl-really-simple-ssl.php
+++ rlrsssl-really-simple-ssl.php
@@ -3,7 +3,7 @@
  * Plugin Name: Really Simple SSL
  * Plugin URI: https://really-simple-ssl.com
  * Description: Lightweight plugin without any setup to make your site SSL proof
- * Version: 4.0.5
+ * Version: 4.0.6
  * Author: Really Simple Plugins
  * Author URI: https://really-simple-plugins.com
  * License: GPL2
