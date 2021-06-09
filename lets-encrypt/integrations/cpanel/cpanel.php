fc2d26ec70ca935bae7d412550fcde2c24287c64
Rogier Lankhorst <rogier@really-simple-plugins.com>
2021-06-09 12:15:18 +0200

    readme

--- lets-encrypt/integrations/cpanel/cpanel.php
+++ lets-encrypt/integrations/cpanel/cpanel.php
@@ -1,32 +1,10 @@
 <?php
 defined( 'ABSPATH' ) or die();
 
-/**
- * Based on the FreeSSL.tech Auto CPanel class
- *
- * @author Anindya Sundar Mandal <anindya@SpeedUpWebsite.info>
- * @copyright  Copyright (C) 2018-2019, Anindya Sundar Mandal
- * @license    http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License, version 3
- * @link       https://SpeedUpWebsite.info
- * @since      Class available since Release 1.0.0
- * 
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * 
- */
 require_once( rsssl_le_path . 'integrations/cpanel/functions.php' );
-
+/**
+ * Completely rebuilt and improved on the FreeSSL.tech Auto CPanel class by Anindya Sundar Mandal
+ */
 class rsssl_cPanel
 {
     public $host;
