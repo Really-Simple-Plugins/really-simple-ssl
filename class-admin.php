4e71776300af77260c181d9f8cc524f49da7b0a5
Rogier Lankhorst <rogierlankhorst@gmail.com>
2021-10-11 13:24:08 +0200

    remove obsolete option + version

--- class-admin.php
+++ class-admin.php
@@ -257,7 +257,7 @@ class rsssl_admin extends rsssl_front_end
                 $this->save_options();
             }
         }
-	    if ( $prev_version && version_compare( $prev_version, '5.1.1', '<=' ) ) {
+	    if ( $prev_version && version_compare( $prev_version, '5.1.3', '<=' ) ) {
 		    if ( get_option( 'rsssl_disable_ocsp' ) ) {
 			    $options = get_option( 'rsssl_options_lets-encrypt' );
                 $options['disable_ocsp'] = true;
