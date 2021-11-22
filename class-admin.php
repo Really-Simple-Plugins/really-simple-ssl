0f34b8c57aaf75263728367857d2aa9f62d7878a
Rogier Lankhorst <rogierlankhorst@gmail.com>
2021-11-22 09:26:19 +0100

    clear transient on save

--- class-admin.php
+++ class-admin.php
@@ -100,6 +100,7 @@ class rsssl_admin extends rsssl_front_end
 	    delete_transient('rsssl_mixed_content_fixer_detected');
 	    delete_transient('rsssl_plusone_count');
 	    delete_transient('rsssl_remaining_task_count');
+	    delete_transient( 'rsssl_can_use_curl_headers_check' );
     }
 
 	/**
