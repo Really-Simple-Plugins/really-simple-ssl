a2780d49ab254ebd2d047d72f854a1e0caff8b3e
mwolters90 <mark@really-simple-ssl.com>
2022-01-18 15:28:51 +0100

    complianz to rsssl text domain

--- lets-encrypt/wizard/notices.php
+++ lets-encrypt/wizard/notices.php
@@ -3,9 +3,9 @@ add_action( 'rsssl_notice_include_alias', 'rsssl_notice_include_alias', 10, 1 );
 function rsssl_notice_include_alias( $args ) {
 	if (!rsssl_is_subdomain() && !RSSSL_LE()->letsencrypt_handler->alias_domain_available() ) {
 		if (strpos(site_url(), 'www.') !== false ) {
-			rsssl_sidebar_notice(  __( "The non-www version of your site does not point to this website. This is recommended, as it will allow you to add it to the certificate as well.", 'complianz-gdpr' ), 'warning' );
+			rsssl_sidebar_notice(  __( "The non-www version of your site does not point to this website. This is recommended, as it will allow you to add it to the certificate as well.", 'really-simple-ssl' ), 'warning' );
 		} else {
-			rsssl_sidebar_notice(  __( "The www version of your site does not point to this website. This is recommended, as it will allow you to add it to the certificate as well.", 'complianz-gdpr' ), 'warning' );
+			rsssl_sidebar_notice(  __( "The www version of your site does not point to this website. This is recommended, as it will allow you to add it to the certificate as well.", 'really-simple-ssl' ), 'warning' );
 		}
 	}
 }
