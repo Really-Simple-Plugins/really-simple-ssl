b42ae72108b47e4680358ca8888114b4b593f106
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-02-01 09:30:53 +0100

    action hook

--- security/server/disable-indexing.php
+++ security/server/disable-indexing.php
@@ -1,26 +1,24 @@
 <?php
 defined( 'ABSPATH' ) or die( "you do not have access to this page!" );
 
-if ( !function_exists( 'rsssl_disable_indexing' ) ) {
-    function rsssl_disable_indexing() {
-
-	    if ( rsssl_get_server() == 'apache' ) {
-		    // Get .htaccess
-		    $htaccess_file = rsssl_htaccess_file();
-		    if ( file_exists( $htaccess_file ) && is_writable( $htaccess_file ) ) {
-			     $htaccess = file_get_contents($htaccess_file);
-				if ( stripos($htaccess, 'options -indexes') !== false ) {
-					return;
-				} else {
-					// insert into .htaccess
-					$rules = "\n" . "Options -Indexes" . "\n";
-					$htaccess = $htaccess . $rules;
-					file_put_contents($htaccess_file, $htaccess);
-				}
-		    }
+function rsssl_disable_indexing() {
+    if ( rsssl_get_server() == 'apache' ) {
+	    // Get .htaccess
+	    $htaccess_file = rsssl_htaccess_file();
+	    if ( file_exists( $htaccess_file ) && is_writable( $htaccess_file ) ) {
+		     $htaccess = file_get_contents($htaccess_file);
+			if ( stripos($htaccess, 'options -indexes') !== false ) {
+				return;
+			} else {
+				// insert into .htaccess
+				$rules = "\n" . "Options -Indexes" . "\n";
+				$htaccess = $htaccess . $rules;
+				file_put_contents($htaccess_file, $htaccess);
+			}
 	    }
+    }
 
-	    if ( rsssl_get_server() == 'nginx' ) {
+    if ( rsssl_get_server() == 'nginx' ) {
 
 //			wrap_headers('nginx') {
 //
@@ -28,11 +26,9 @@ if ( !function_exists( 'rsssl_disable_indexing' ) ) {
 //		    server {
 //			    location /{anydir} {
 //				    autoindex off;
-			//  }
-			//}
+		//  }
+		//}
 
-	    }
     }
 }
-
-rsssl_disable_indexing();
+add_action('plugins_loaded', 'rsssl_disable_indexing');
