583eb2698b7a1ec6320a3ecc4a7cbfe86e6facb3
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-03-07 16:28:24 +0100

    stash

--- settings/templates/admin-wrap.php
+++ settings/templates/admin-wrap.php
@@ -1,12 +1,5 @@
 <?php defined('ABSPATH') or die("you do not have access to this page!"); ?>
 <div id="rsssl">
-    <div class="rsssl-secondary-header-item">
-        <div class="rsssl-save-settings-feedback" style="display: none;">
-            <?php _e("Save settings" , "really-simple-ssl") ?>
-        </div>
-        <div class="rsssl-instructions">{instructions}</div>
-    </div>
-
     <div id="rsssl-content-area">
         {content}
     </div>
