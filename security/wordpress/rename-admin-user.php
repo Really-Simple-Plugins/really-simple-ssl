f358eb55ae7105df0ed2d3b705d000395d065742
mwolters90 <mark@really-simple-ssl.com>
2022-04-14 13:24:31 +0200

    rename user 'admin' + multisite

--- security/wordpress/rename-admin-user.php
+++ security/wordpress/rename-admin-user.php
@@ -1,7 +1,29 @@
 <?php
 defined( 'ABSPATH' ) or die( "you do not have access to this page!" );
 
-add_action('admin_init', 'rsssl_rename_admin_user');
+if ( is_admin() ) {
+//	add_filter('rsssl_notices', 'rsssl_admin_username_changed', 50, 20);
+}
+
+/**
+ * @return void
+ *
+ * Username 'admin' changed notice
+ */
+function rsssl_admin_username_changed( $notices ) {
+	$notices['username_admin_changed'] = array(
+		'condition' => 'rsssl_username_admin_changed',
+		'callback' => '_true_',
+		'score' => 5,
+		'output' => array(
+			'_true_' => array(
+				'msg' => sprintf(__("Username 'admin' has been changed to %s", "really-simple-ssl"), get_option('rsssl_new_admin_username') ),
+				'icon' => 'open',
+				'dismissible' => true,
+			),
+		),
+	);
+}
 /**
  * @return void
  *
@@ -9,83 +31,70 @@ add_action('admin_init', 'rsssl_rename_admin_user');
  */
 function rsssl_rename_admin_user() {
 
-//    if ( rsssl_get_option('rsssl_rename_admin_user') == '1' && ! get_option('rsssl_admin_user_updated') ) {
+    // Get user data for login admin
+	$admin_user = get_user_by('login','admin');
 
-        // Get user data for admin
-		$admin_user = get_user_by('login','admin');
+	if ( $admin_user ) {
+		$admin_user_id  = $admin_user->data->ID;
+		$admin_userdata = get_userdata( $admin_user_id );
+		$admin_email    = $admin_userdata->data->user_email;
 
-		if ( $admin_user ) {
-			$admin_user_id  = $admin_user->data->ID;
-			$admin_userdata = get_userdata( $admin_user_id );
-			$login          = $admin_userdata->data->user_login;
-			$admin_email    = $admin_userdata->data->user_email;
+		if ( current_user_can( 'manage_options' ) ) {
 
-			if ( current_user_can( 'manage_options' ) && $login === 'admin' ) {
-				error_log( print_r( $admin_userdata, true ) );
+			// update e-mail for existing user. Cannot have two accounts connected to the same e-mail address
+			wp_update_user( array(
+				'ID'         => $admin_user_id,
+				'user_email' => 'temp@example.com',
+			) );
 
-				// update e-mail for existing user
-				wp_update_user( array(
-					'ID'         => $admin_user_id,
-					'user_email' => 'temp@example.com',
-				) );
+			// Generate new user login. Do it here so we can get the ID for this user later in the function
+			$new_user_login = rsssl_generate_random_string( 12 );
+			update_option('rsssl_new_admin_username', $new_user_login);
 
-				$new_userdata = array(
-//				'ID'                    => '',    //(int) User ID. If supplied, the user will be updated.
-					'user_pass'            => rsssl_generate_random_string( 12 ),
-					//(string) The plain-text user password.
-					'user_login'           => rsssl_generate_random_string( 12 ),
-					//(string) The user's login username.
-					'user_nicename'        => $admin_user->data->user_nicename,
-					//(string) The URL-friendly user name.
-					'user_url'             => '',
-					//(string) The user URL.
-					'user_email'           => $admin_email,
-					//(string) The user email address.
-					'display_name'         => $admin_user->data->display_name,
-					//(string) The user's display name. Default is the user's username.
-					'nickname'             => '',
-					//(string) The user's nickname. Default is the user's username.
-					'first_name'           => '',
-					//(string) The user's first name. For new users, will be used to build the first part of the user's display name if $display_name is not specified.
-					'last_name'            => '',
-					//(string) The user's last name. For new users, will be used to build the second part of the user's display name if $display_name is not specified.
-					'description'          => '',
-					//(string) The user's biographical description.
-					'rich_editing'         => '',
-					//(string|bool) Whether to enable the rich-editor for the user. False if not empty.
-					'syntax_highlighting'  => '',
-					//(string|bool) Whether to enable the rich code editor for the user. False if not empty.
-					'comment_shortcuts'    => '',
-					//(string|bool) Whether to enable comment moderation keyboard shortcuts for the user. Default false.
-					'admin_color'          => '',
-					//(string) Admin color scheme for the user. Default 'fresh'.
-					'use_ssl'              => '',
-					//(bool) Whether the user should always access the admin over https. Default false.
-					'user_registered'      => '',
-					//(string) Date the user registered. Format is 'Y-m-d H:i:s'.
-					'show_admin_bar_front' => '',
-					//(string|bool) Whether to display the Admin Bar for the user on the site's front end. Default true.
-					'role'                 => $admin_user->roles[0],
-					//(string) User's role.
-					'locale'               => '',
-					//(string) User's locale. Default empty.
-				);
+			// Populate the new user data. Use current 'admin' userdata wherever available
+			$new_userdata = array(
+				'user_pass'            => rsssl_generate_random_string( 12 ), //(string) The plain-text user password.
+				'user_login'           => $new_user_login, //(string) The user's login username.
+				'user_nicename'        => isset( $admin_user->data->user_nicename ) ? $admin_user->data->user_nicename : '', //(string) The URL-friendly user name.
+				'user_url'             => isset( $admin_user->data->user_url ) ? $admin_user->data->user_url : '', //(string) The user URL.
+				'user_email'           => isset( $admin_email ) ? $admin_email : '', //(string) The user email address.
+				'display_name'         => isset( $admin_user->data->display_name ) ? $admin_user->data->display_name : '', //(string) The user's display name. Default is the user's username.
+				'nickname'             => isset( $admin_user->data->nickname ) ? $admin_user->data->nickname : '', //(string) The user's nickname. Default is the user's username.
+				'first_name'           => isset( $admin_user->data->user_firstname ) ? $admin_user->data->user_firstname : '', //(string) The user's first name. For new users, will be used to build the first part of the user's display name if $display_name is not specified.
+				'last_name'            => isset( $admin_user->data->user_lastname ) ? $admin_user->data->user_lastname : '', //(string) The user's last name. For new users, will be used to build the second part of the user's display name if $display_name is not specified.
+				'description'          => isset( $admin_user->data->description ) ? $admin_user->data->description : '', //(string) The user's biographical description.
+				'rich_editing'         => isset( $admin_user->data->rich_editing ) ? $admin_user->data->rich_editing : '', //(string|bool) Whether to enable the rich-editor for the user. False if not empty.
+				'syntax_highlighting'  => isset( $admin_user->data->syntax_highlighting ) ? $admin_user->data->syntax_highlighting : '', //(string|bool) Whether to enable the rich code editor for the user. False if not empty.
+				'comment_shortcuts'    => isset( $admin_user->data->comment_shortcuts ) ? $admin_user->data->comment_shortcuts : '', //(string|bool) Whether to enable comment moderation keyboard shortcuts for the user. Default false.
+				'admin_color'          => isset( $admin_user->data->admin_color ) ? $admin_user->data->admin_color : '', //(string) Admin color scheme for the user. Default 'fresh'.
+				'use_ssl'              => isset( $admin_user->data->use_ssl ) ? $admin_user->data->use_ssl : '', //(bool) Whether the user should always access the admin over https. Default false.
+				'user_registered'      => isset( $admin_user->data->user_registered ) ? $admin_user->data->user_registered : '', //(string) Date the user registered. Format is 'Y-m-d H:i:s'.
+				'show_admin_bar_front' => isset( $admin_user->data->show_admin_bar_front ) ? $admin_user->data->show_admin_bar_front : '', //(string|bool) Whether to display the Admin Bar for the user on the site's front end. Default true.
+				'role'                 => isset( $admin_user->roles[0] ) ? $admin_user->roles[0] : '', //(string) User's role.
+				'locale'               => isset( $admin_user->data->locale ) ? $admin_user->data->locale : '', //(string) User's locale. Default empty.
+			);
 
-				// Replace admin user with new admin user
-				error_log( "create user" );
+			// Create new admin user
+			wp_insert_user( $new_userdata );
 
-				// 5YvQ^VC^RT685^Th^D1gKyE(
-//            wp_create_user( rsssl_generate_random_string(12), rsssl_generate_random_string(12), $admin_email );
+			// Delete old user and attribute posts to new user ID
+			$new_user = get_user_by('login',$new_user_login);
+			wp_delete_user( $admin_user_id, $new_user->data->ID);
 
-				wp_insert_user( $new_userdata );
-				// Attribute posts to new user
+			// On multisite we have to update the $wpdb->prefix . sitemeta -> meta_key -> site_admins -> meta_value to the new username
+			if ( is_multisite() ) {
 
-				// Delete old user
+				global $wpdb;
 
-//            update_option('rsssl_admin_user_updated', true);
+				$site_admins = $wpdb->query("SELECT 'meta_value' FROM `wp_sitemeta` WHERE `meta_key` = 'site_admins'");
+				$site_admins = str_replace('admin', $new_user_login, $site_admins);
+				$wpdb->query("UPDATE `wp_sitemeta` SET `meta_value` = $site_admins WHERE `meta_key` = 'site_admins'");
+
 			}
+
+			update_option('rsssl_username_admin_changed', true );
 		}
-//    }
+	}
 }
 
 /**
@@ -103,4 +112,17 @@ function rsssl_has_admin_user() {
 	}
 
     return false;
+}
+
+/**
+ * @return bool
+ *
+ * Notice condition
+ */
+function rsssl_username_admin_changed() {
+	if ( get_option('rsssl_username_admin_changed') ) {
+		return true;
+	}
+
+	return false;
 }
