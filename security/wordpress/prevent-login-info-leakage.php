b4f985a2c04ef205d088e46f84be8f19e7bd97eb
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-05-19 13:58:10 +0200

    login-info-leakage

--- security/wordpress/prevent-login-info-leakage.php
+++ security/wordpress/prevent-login-info-leakage.php
@@ -1,8 +1,6 @@
 <?php
 defined( 'ABSPATH' ) or die( "you do not have access to this page!" );
 
-add_filter( 'login_errors', 'rsssl_no_wp_login_errors' );
-
 /**
  * @return string|void
  *
@@ -12,3 +10,4 @@ function rsssl_no_wp_login_errors()
 {
     return __("Invalid login details.", "really-simple-ssl");
 }
+add_filter( 'login_errors', 'rsssl_no_wp_login_errors' );
