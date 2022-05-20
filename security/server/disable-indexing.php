9b897bb765b3d0b0d5aff23510cb0a74becb4b28
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-05-20 16:29:33 +0200

    nignxin

--- security/server/disable-indexing.php
+++ security/server/disable-indexing.php
@@ -11,46 +11,7 @@ function rsssl_disable_indexing_rules( $rules ) {
 	return $rules;
 }
 add_filter('rsssl_htaccess_security_rules', 'rsssl_disable_indexing_rules');
-/**
- * @param $notices
- *
- * @return mixed
- *
- * Show notice on NGINX for disabling directory indexing
- */
-function rsssl_indexing_nginx_notice( $notices ) {
-	if ( rsssl_get_server() == 'nginx' ) {
-		$notices['indexing-nginx'] = array(
-			'callback' => '_true_',
-			'score'    => 5,
-			'output'   => array(
-				'true' => array(
-					'msg'         => __( "The code to block indexing cannot be added automatically on nginx. Add the following code to your nginx.conf server block:", "really-simple-ssl" )
-					                 . "<br>" . rsssl_indexing_nginx_code(),
-					'icon'        => 'open',
-					'dismissible' => true,
-				),
-			),
-		);
-	}
-
-	return $notices;
-}
-add_filter('rsssl_notices', 'rsssl_indexing_nginx_notice');
 
 /**
- * @return string
- *
- * Return NGINX code to block indexing
+ * Dropped suggestions for indexing in NGINX as indexing in NGINX is by default disabled.
  */
-function rsssl_indexing_nginx_code() {
-	$code = '<code>';
-	$code .= 'server {' . '<br>';
-	$code .= '&nbsp;&nbsp;&nbsp;&nbsp;location /{anydir} {' . '<br>';
-	$code .= '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;autoindex off;' . '<br>';
-	$code .= '&nbsp;&nbsp;&nbsp;&nbsp;}' . '<br>';
-	$code .= '}';
-	$code .= '</code>';
-
-	return $code;
-}
--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-polyfill'), 'version' => 'a8e2aa8b41533ecd724cd0078993ee84');
+<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-polyfill'), 'version' => 'c9e6f2bd4628e8c2d5ee0ce563a399f4');
