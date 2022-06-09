2355d8a22dabf29ee6a0e30f19e35b931e1e57c9
Mark-met-een-K <40232045+mwolters90@users.noreply.github.com>
2022-06-09 12:59:38 +0200

    hide error on password reset

--- security/wordpress/prevent-login-info-leakage.php
+++ security/wordpress/prevent-login-info-leakage.php
@@ -2,12 +2,27 @@
 defined( 'ABSPATH' ) or die( "you do not have access to this page!" );
 
 /**
- * @return string|void
- *
  * Override default login error message
- */
+ * @return string|void
+ **/
 function rsssl_no_wp_login_errors()
 {
     return __("Invalid login details.", "really-simple-ssl");
 }
 add_filter( 'login_errors', 'rsssl_no_wp_login_errors' );
+
+/**
+ * Hide feedback entirely on password reset (no filter available).
+ *
+ * @return void
+ *
+ */
+function rsssl_hide_pw_reset_error() {
+    ?>
+    <style>
+       .login-action-lostpassword #login_error{
+           display: none;
+       }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'rsssl_hide_pw_reset_error' ); ?>
