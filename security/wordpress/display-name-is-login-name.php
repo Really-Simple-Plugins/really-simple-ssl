3da4e8eb1327ec832aab0df41f9f2489758f9483
mwolters90 <mark@really-simple-ssl.com>
2022-06-10 13:22:23 +0200

    hide .error

--- security/wordpress/display-name-is-login-name.php
+++ security/wordpress/display-name-is-login-name.php
@@ -10,53 +10,59 @@ add_action( 'wp_error_added', 'rsssl_handle_wp_error', 15, 4 );
  * @param $user_id
  * @param $userdata
  *
- * @return false|mixed
+ * @return false
  *
  * Filter user registration
  */
 function rsssl_pre_insert_user_data_filter( $data, $update, $user_id, $userdata ) {
-    // If login = display_name, do not add user
-    error_log( print_r($data, true ) );
 
-    if ($data->user_login === $data->display_name) {
-//        add_filter('registration_errors', 'filter_registration_errors');
-
-        // Add notice
-        error_log("Login equals display name");
-        return false;
-    }
-
-    return false;
+	if ( is_object( $data ) ) {
+		if ( $data->user_login === $data->display_name ) {
+            // Do not add user! Login equals display name
+			return false;
+		}
+	}
 }
 
-add_filter('registration_errors', 'filter_registration_errors');
-function filter_registration_errors($error){
-    error_log("Reg err cb");
-    if ( $error->get_error_messages( 'empty_data' ) ) {
-        $error = new WP_Error( 'empty_data', "Blelellelele. Thanks!" );
-    }
-    return $error;
-}
-
+/**
+ *
+ * Add a custom
+ * @param $code
+ * @param $message
+ * @param $data
+ * @param $wp_error
+ *
+ * @return void
+ */
 function rsssl_handle_wp_error($code, $message, $data, $wp_error) {
-    error_log("Error!");
-
-//    (new WP_Error)->remove( $code );
-//    return new WP_Error( 'empty_data', __('Login name equals display name.", "really-simple-ssl" ) );
-
-//    error_log(print_r($wp_error, true));
-//    error_log(print_r($code, true));
-//    error_log(print_r($data, true));
-//    error_log(print_r($message, true));
-
-
-    if ( $code === 'empty_data' ) {
-        error_log("Empty data");
-        $message = 'yes';
-
-        return $message;
-        // Show different error message. But how?
-//        return new WP_Error('empty_data', __('Login name equals display name.', 'really-simple-ssl'));
-    }
-
+	if ( $code === 'empty_data' ) {
+        error_log("Add custom error");
+		// Add custom error
+		// After h1 #add-new-user
+        // Do not use 'error' class as it's hidden by JS
+	}
 }
+
+/**
+ *
+ * Add javascript to hide error on add user page
+ *
+ * @return void
+ */
+function rsssl_hide_error_js() {
+	?>
+		<script>
+            if ( document.querySelector('.error') ) {
+                document.querySelector('.error').style['display'] = 'none';
+                document.querySelector('.user-new-php .error').style['display'] = 'none';
+            }
+		 </script >
+    <style>
+        .user-new-php .error {
+            display: none;
+        }
+    </style>
+    <?php
+}
+
+add_action('admin_print_scripts', 'rsssl_hide_error_js');
