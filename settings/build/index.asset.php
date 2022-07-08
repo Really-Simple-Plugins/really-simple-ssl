05417b12b5ecdea35bddcce3588d29a9e4b3e63f
Hessel de Jong <info@hesseldejong.com>
2022-07-08 10:16:18 +0200

    Merge from 6.0-development-design

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'b5ab0d58d77fd28dd31b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '8f3448f4d9c329c85b6f');
