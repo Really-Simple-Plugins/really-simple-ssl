5aebd33b61c74bbee757e95ee5a1afe6470306cf
mwolters90 <mark@really-simple-ssl.com>
2022-09-02 09:45:42 +0200

    build

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'e3eccf1bd6904b8022c4382318203111');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '4031d8494180b22b9116');
