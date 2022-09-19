091a6a1259adfbf1165edc69d4e12beb2dfe8752
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-09-19 12:54:43 +0200

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,20 @@ const concat = require('gulp-concat');
 const cssbeautify = require('gulp-cssbeautify');
 const cssuglify = require('gulp-uglifycss');
 const jsuglify = require('gulp-uglify');
-const sass = require('gulp-sass')(require('node-sass'));
+const sass = require('gulp-sass')(require('sass'));
 const spawn = require('child_process').spawn;
 
 function scssTask(cb) {
   // compile scss to css and minify
   gulp.src('./assets/css/admin.scss')
-  .pipe(sass(({outputStyle: 'uncompressed'})).on('error', sass.logError))
+  .pipe(sass(({outputStyle: 'expanded'})).on('error', sass.logError))
   .pipe(cssbeautify())
   .pipe(gulp.dest('./assets/css'))
   .pipe(cssuglify())
   .pipe(concat('admin.min.css'))
   .pipe(gulp.dest('./assets/css'));
 
-  cb();
+   cb();
 }
 exports.scss = scssTask
 
