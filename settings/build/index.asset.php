765da5767523d59bff85d2c4d8383583025cbcb6
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-09-20 19:20:11 +0200

    fix html in notice +toggle + title notices

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'b365e4c7e8a7a9b71e50');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'c81da0e3debe3842e86a');
--- settings/src/Settings/Settings.js
+++ settings/src/Settings/Settings.js
@@ -15,6 +15,19 @@ import { __ } from '@wordpress/i18n';
 class Settings extends Component {
     constructor() {
         super( ...arguments );
+        this.state = {
+            noticesShown:true,
+        };
+    }
+
+    toggleNotices(){
+        const {
+            noticesShown,
+        } = this.state;
+
+        this.setState({
+            noticesShown:!noticesShown,
+        });
     }
 
     render() {
@@ -25,6 +38,9 @@ class Settings extends Component {
         let selectedStep = this.props.selectedStep;
         let menu = this.props.menu;
         const { menu_items: menuItems } = menu;
+        const {
+            noticesShown,
+        } = this.state;
 
         if ( ! isAPILoaded ) {
             return (
@@ -126,7 +142,13 @@ class Settings extends Component {
                     </div>
                 </div>
                 <div className="rsssl-wizard-help">
-                    {notices.map((field, i) => <Help key={i} index={i} help={field} fieldId={field.id}/>)}
+                    <div className="rsssl-help-header">
+                        <div className="rsssl-help-title">
+                            {__("Notifications", "really-simple-ssl")}
+                        </div>
+                        <div className="rsssl-help-control" onClick={ () => this.toggleNotices() }>{__("Toggle/hide all","really-simple-ssl")}</div>
+                    </div>
+                    {notices.map((field, i) => <Help key={i} noticesShown={noticesShown} index={i} help={field} fieldId={field.id}/>)}
                 </div>
             </Fragment>
         )
