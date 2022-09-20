e98b73214b092e29dec83ed5bd713663ee3189f9
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-09-20 19:40:54 +0200

    fix system status

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'c81da0e3debe3842e86a');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '9d15de7f7c9ac22ca460');
