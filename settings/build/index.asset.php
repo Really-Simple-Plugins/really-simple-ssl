78ff0026d9050a0132969385157482679afc8346
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-09-20 17:31:03 +0200

    fix learning mode css

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '1135dbf0b02967820cc7');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'b365e4c7e8a7a9b71e50');
