179eae517c4277e69e331a087e5337c97ff01b2f
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-10-03 11:20:46 +0200

    don't update fallback method.

--- functions.php
+++ functions.php
@@ -24,12 +24,12 @@ function rsssl_get_option( string $name, $default=false ) {
 
 	//fallback, will be removed after 6.2
 	//because we only check if the option is not saved in the new style, this if should normally never get executed.
-//	if (
-//		!isset($options[$name]) &&
-//		($name === 'ssl_enabled' || $name === 'redirect' || $name === "mixed_content_fixer")
-//	) {
-//		$options = rsssl_get_legacy_option($options, $name);
-//	}
+	if (
+		!isset($options[$name]) &&
+		($name === 'ssl_enabled' || $name === 'redirect' || $name === "mixed_content_fixer")
+	) {
+		$options = rsssl_get_legacy_option($options, $name);
+	}
 
 	$value = $options[ $name ] ?? false;
 	if ( $value===false && $default!==false ) {
@@ -61,7 +61,7 @@ function rsssl_is_networkwide_active(){
 }
 
 /**
- * if the option is does not exist in our new array, check if it's available in the old option. If so, use that one, and drop it from the old options.
+ * if the option is does not exist in our new array, check if it's available in the old option. If so, use that one
  * @deprecated to be used until 6.2, as fallback for failed upgrades
  * @param array  $options
  * @param string $name
@@ -71,46 +71,20 @@ function rsssl_is_networkwide_active(){
 function rsssl_get_legacy_option( array $options, string $name): array {
 	$old_options = is_multisite() ? get_site_option('rlrsssl_network_options') : get_option( 'rlrsssl_options' );
 	if ( $old_options ) {
-		$can_update = is_admin() && rsssl_user_can_manage();
 		if ( $name === 'ssl_enabled' && isset( $old_options['ssl_enabled']) ) {
 			$val                    = $old_options['ssl_enabled'];
 			$options['ssl_enabled'] = $val;
-			if ( $can_update ) {
-				rsssl_update_option( 'ssl_enabled', $val );
-				unset( $old_options['ssl_enabled'] );
-				update_option( 'rlrsssl_options', $old_options, false );
-			}
 		} else if ( $name === 'dismiss_all_notices' && isset( $old_options['dismiss_all_notices']) ) {
 			$val = $old_options['dismiss_all_notices'];
 			$options['dismiss_all_notices'] = $val;
-			if ( $can_update ) {
-				rsssl_update_option( 'dismiss_all_notices', $val );
-				unset( $old_options['dismiss_all_notices'] );
-				update_option( 'rlrsssl_options', $old_options, false );
-			}
 		} else if ( $name === 'mixed_content_fixer' && isset($old_options['autoreplace_insecure_links']) ) {
 			$val = $old_options['autoreplace_insecure_links'];
 			$options['mixed_content_fixer'] = $val;
-			if ( $can_update ) {
-				rsssl_update_option( $name, $val );
-				unset( $old_options['autoreplace_insecure_links'] );
-				update_option( 'rlrsssl_options', $old_options, false );
-			}
 		} else if ( $name === 'redirect' ){
 			if ( isset($old_options['htaccess_redirect']) && $old_options['htaccess_redirect'] ) {
 				$options['redirect'] = 'htaccess';
-				if ( $can_update ) {
-					rsssl_update_option( 'redirect', 'htaccess' );
-					unset( $old_options['htaccess_redirect'] );
-					update_option( 'rlrsssl_options', $old_options, false );
-				}
 			} else if (isset($old_options['wp_redirect']) && $old_options['wp_redirect']) {
 				$options['redirect'] = 'wp_redirect';
-				if ( $can_update ) {
-					rsssl_update_option( $name, 'wp_redirect' );
-					unset( $old_options['wp_redirect'] );
-					update_option( 'rlrsssl_options', $old_options, false );
-				}
 			}
 		}
 	}
