332b07774bea4713950f67329035ef93397da1a3
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-10-18 17:46:33 +0200

    CSP compatible build.

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'dc7cec99860f8bee9051');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '4b714798bf9fea3708b9');
