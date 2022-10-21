89b9ca2a99a1ab4c6d7add4bb321675333947e79
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-10-21 10:01:51 +0200

    css fix

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '4b714798bf9fea3708b9');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'e4b1e234fbe76fd40056');
