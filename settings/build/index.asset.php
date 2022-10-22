f984b4ec92255b5f5410d26846dbff21ecc7b04b
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-10-22 23:18:14 +0200

    text change

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'c7ae4a0fdfd3369cd536');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '7007de2a179802e0ba98');
