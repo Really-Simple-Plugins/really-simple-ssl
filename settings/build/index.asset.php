341059a025f21b5a136a74387bc47b6e8d8956ea
Hessel de Jong <info@hesseldejong.com>
2022-10-24 12:19:19 +0200

    Responsive changes

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '7007de2a179802e0ba98');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '9a89e64bc5b243eaa868');
