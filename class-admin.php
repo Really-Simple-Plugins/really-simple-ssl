14cc548197432f63229be1e706ed031fdc773c16
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-10-26 12:01:53 +0200

    black friday

--- class-admin.php
+++ class-admin.php
@@ -125,13 +125,13 @@ class rsssl_admin
 	    if ( defined("rsssl_pro_version" ) ) {
             return false;
         }
-	    $start_day = 22;
-        $end_day = 29;
+	    $start_day = 21;
+        $end_day = 28;
 	    $current_year = date("Y");//e.g. 2021
 	    $current_month = date("n");//e.g. 3
         $current_day = date("j");//e.g. 4
 
-        if ( $current_year == 2021 && $current_month == 11 &&
+        if ( $current_year == 2022 && $current_month == 11 &&
              $current_day >=$start_day &&
              $current_day <= $end_day
         ) {
@@ -2218,7 +2218,7 @@ class rsssl_admin
 	            ),
             ),
 
-            'bf_notice' => array(
+            'bf_notice2022' => array(
 	            'condition'  => array(
                         'RSSSL()->admin->is_bf'
                 ),
