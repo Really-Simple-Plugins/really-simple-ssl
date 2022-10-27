54af3979b0eada20901ade13b5df72f0a01360d8
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-10-27 16:48:38 +0200

    fix next page on multisite

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '5e866b2108d375f3d936');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '753b85b901200b7c107f');
