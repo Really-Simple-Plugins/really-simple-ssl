6285f6f1502dccbec65a527b02824c38df58ce43
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-10-27 11:09:40 +0200

    stash

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '227fe8677f0debeed6ed');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'f7468bffbb8ee3685fc2');
