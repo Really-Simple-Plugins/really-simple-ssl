758bb42fc9c0d0aa09d25a5e2ddae7bf44d73a04
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-11-01 09:31:14 +0100

    version check wp

--- rlrsssl-really-simple-ssl.php
+++ rlrsssl-really-simple-ssl.php
@@ -245,7 +245,7 @@ if ( ! function_exists( 'rsssl_user_can_manage' ) ) {
 function RSSSL()
 {
 	global $wp_version;
-	if ( version_compare($wp_version, '4.9', '>=') && version_compare(PHP_VERSION, '7.2', '>=')) {
+	if ( version_compare($wp_version, '5.7', '>=') && version_compare(PHP_VERSION, '7.2', '>=')) {
 		return REALLY_SIMPLE_SSL::instance();
 	}
 }
