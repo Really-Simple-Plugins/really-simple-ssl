6a094a62adae2063c6e69efbf22d44b63db8b857
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-11-15 12:04:36 +0100

    css fix for DNS

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'aa65bd4f3e6a34410755');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'cc974f54171786a373c4');
