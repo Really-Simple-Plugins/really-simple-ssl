4cff32c9c1646af6d5368ee8bca3537d51369d9d
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-12-07 19:15:18 +0100

    general changes

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'c29f6f34369ace1ff784');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '7f5475ce68e294266ab2');
