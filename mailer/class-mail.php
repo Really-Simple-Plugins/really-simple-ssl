9c0e7e53fe94504aea6a42d1403e08df65e6756d
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-12-08 10:24:24 +0100

    stash

--- mailer/class-mail.php
+++ mailer/class-mail.php
@@ -9,6 +9,7 @@ if ( !class_exists('rsssl_mailer') ) {
 	class rsssl_mailer {
 
 		public $to;
+		public $title;
 		public $headers;
 		public $message;
 		public $subject;
@@ -26,6 +27,10 @@ if ( !class_exists('rsssl_mailer') ) {
 
 			if (empty($this->to) || empty($this->message) || empty($this->subject) ) {
 				return false;
+			}
+
+			if (empty($this->title)) {
+				$this->title = __("Learn more about our features!", "really-simple-ssl");
 			}
 
 			if ( !is_email($this->to) ){
@@ -51,8 +56,8 @@ if ( !class_exists('rsssl_mailer') ) {
 			}
 
 			$body = str_replace(
-				['{message}','{warnings}','email-address','learn-more'],
-				[ wp_kses_post($this->message), $block_html, $this->to, __("Learn more",'really-simple-ssl') ],
+				['{title}','{message}','{warnings}','email-address','learn-more'],
+				[ sanitize_text_field($this->title), wp_kses_post($this->message), $block_html, $this->to, __("Learn more",'really-simple-ssl') ],
 				$template);
 			$success = wp_mail( $this->to, sanitize_text_field($this->subject), $body, array('Content-Type: text/html; charset=UTF-8') );
 			set_transient('rsssl_email_recently_sent', true, 5 * MINUTE_IN_SECONDS );
