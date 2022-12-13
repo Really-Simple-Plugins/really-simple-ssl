4deac108f79419eeb06e1d9d3973b64f2f52e47b
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-12-13 10:10:23 +0100

    premium bullets on <1080px screen

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '8f3753161382f223ae79');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'b2ede32192c234ce7b9e');
