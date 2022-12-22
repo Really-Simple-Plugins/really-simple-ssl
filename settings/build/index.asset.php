8d3f6cf686791590e58c4ff228ff43459f1650a2
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-12-22 08:55:25 +0100

    restructured

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'c9ad95a3b9b1b1bc05f6');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '5ee6e3fb0b591934f1f0');
