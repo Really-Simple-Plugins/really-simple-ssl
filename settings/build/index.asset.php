98a3a157f05acc4d6b680ee5c6aece424faeb7b0
Rogier Lankhorst <rogierlankhorst@gmail.com>
2022-12-30 08:50:08 +0100

    some functions to functional components

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '824f730e60781f17799a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '91c1864cc9e0167bed4c');
