0913d3d89d70430d1e3ab79764f10d13f7dd5aee
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-01-03 09:00:19 +0100

    site url in case of mixed content

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '3d4466522b294d6fa668');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '9c8475307ad5b3ed28eb');
