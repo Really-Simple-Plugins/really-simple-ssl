a95e55094cfcadee092fb84eca2b00f126975a51
mwolters90 <mark@really-simple-ssl.com>
2023-01-04 09:40:14 +0100

    correct plugin file name

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -28,7 +28,7 @@ require_once "{$_tests_dir}/includes/functions.php";
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/really-simple-ssl.php';
+	require dirname( __FILE__, 2 ) . '/rlrsssl-really-simple-ssl.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
