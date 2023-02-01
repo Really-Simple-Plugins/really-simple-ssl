17e9e80411a52c14fe8ad0240a5525880843c4f1
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-02-01 12:44:06 +0100

    build

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'f6d2616026f4b1d4ebc9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '8bfef548c43639730535');
