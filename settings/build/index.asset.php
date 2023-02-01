58c251cbbe140240216b404039d2cd617a2b1384
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-02-01 14:00:15 +0100

    remove lable

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'd32a008833e45b09a5b3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'a03141fabd4151f3d2b6');
