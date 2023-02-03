e19d74d66348acc85ef301e8f79690498ce157fb
Marcel Santing <marcel@really-simple-plugins.com>
2023-02-03 16:01:41 +0100

    finally was able to develop some further after a lot of DB testing

--- security/wordpress/class-vulnerabilities.php
+++ security/wordpress/class-vulnerabilities.php
@@ -20,7 +20,6 @@ if (!class_exists("rsssl_vulnerabilities")) {
 
         function __construct()
         {
-            add_action('admin_init', array($this, 'check_db_values'));
             add_action('admin_init', array($this, 'download_vulnerabilities'));
         }
 
@@ -33,9 +32,6 @@ if (!class_exists("rsssl_vulnerabilities")) {
         {
             //downloads vulnerabilities
             $this->download_vulnerabilities();
-
-            //enables wp cron for incremental updates
-            $this->enable_wp_cron();
         }
 
         public static function instance()
@@ -48,120 +44,81 @@ if (!class_exists("rsssl_vulnerabilities")) {
         }
 
         /**
-         * Checks if the database table exists, if not, create it. If it does, check if the columns are still there.
-         *
-         * TODO: if only on method is called in this function rework this.
-         * @return void
-         */
-        public function check_db_values()
-        {
-            $this->check_for_db_values();
-        }
-
-        /**
          * Checks for vulnerabilities in the core, plugins and themes.
          *
          * @return void
          */
         public function download_vulnerabilities()
         {
-            //TODO: check if we download full or incremental based on last download
-            //downloads wp core only based on current installed version
-            $this->check_for_core_vulnerabilities();
-            //downloads plugins and themes
-            $this->check_for_plugin_vulnerabilities();
+            //first we download the core vulnerabilities
+            $this->download_core_vulnerabilities();
+
+            //then we download the plugin vulnerabilities
+            $this->download_plugin_vulnerabilities();
         }
 
-        private function check_for_core_vulnerabilities()
+        /**
+         * Checks if the file is valid
+         *
+         * @param bool $isCore
+         * @return bool
+         */
+        private function validate_local_file(bool $isCore = false): bool
         {
-            $core_vulnerabilities = $this->get_core_vulnerabilities();
-            $this->save_core_vulnerabilities($core_vulnerabilities);
-        }
+            $isCore ? $file = 'core.json' : $file = 'components.json';
+            $upload_dir = wp_upload_dir();
+            $upload_dir = $upload_dir['basedir'];
+            $upload_dir = $upload_dir . '/rsssl';
+            $file = $upload_dir . '/' . $file;
 
-        private function check_for_db_values()
-        {
-            //first we check if the table vulnerability exists
-            global $wpdb;
-            $table_name = $wpdb->prefix . "rsssl_vulnerabilities";
-            if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
-                //table does not exist, create it
-                $this->create_table();
-            } else {
-                //the table exists, check if the columns are still there
-                $this->check_columns($table_name);
+            if (file_exists($file)) {
+                //now we check if the file is older than 3 days, if so, we download it again
+                $file_time = filemtime($file);
+                $now = time();
+                $diff = $now - $file_time;
+                $days = floor($diff / (60 * 60 * 24));
+                if ($days < 3) {
+                    return true;
+                }
             }
-
-
-        }
-
-        private function create_table()
-        {
-            global $wpdb;
-            $table_name = $wpdb->prefix . "rsssl_vulnerabilities";
-            $charset_collate = $wpdb->get_charset_collate();
-            $sql = "CREATE TABLE $table_name (
-                id mediumint(9) NOT NULL AUTO_INCREMENT,
-                component varchar(255) NOT NULL,
-                version varchar(255) NOT NULL,
-                updated datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-                severity_critical int(5) DEFAULT 0 NOT NULL,
-                severity_high int(5) DEFAULT 0 NOT NULL,
-                severity_medium int(5) DEFAULT 0 NOT NULL,
-                severity_low int(5) DEFAULT 0 NOT NULL,
-                severity_unknown int(5) DEFAULT 0 NOT NULL,
-                last_checked datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-                PRIMARY KEY  (id)
-            ) $charset_collate;";
-            require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-            dbDelta($sql);
+            return false;
         }
 
         /**
          * Downloads the vulnerabilities for the current core version.
          *
-         * TODO: check if we download full or incremental based on last download
-         * @return mixed|null
+         * @return void
          */
-        private function get_core_vulnerabilities()
+        private function download_core_vulnerabilities(): void
         {
             global $wp_version;
-            //first we download the correct json from 'api.really-simple-security.com'
-//            $url = 'https://api.really-simple-security.com/downloads/wp-core_' . $wp_version . '.json';
-            $url = 'https://api.really-simple-security.com/storage/downloads/wp-core_6.0.1.json';
-            return $this->download($url);
+            $wp_version = '6.0.1';
+            $url = 'https://api.really-simple-security.com/storage/downloads/core/wp-core_' . $wp_version . '.json';
+            $data = $this->download($url);
 
+            //we convert the data to an array
+            $data = json_decode(json_encode($data), true);
+
+             //first we store this as a json file in the uploads folder
+            $this->store_file($data, true);
         }
-        private function check_for_plugin_vulnerabilities()
+
+        private function download_plugin_vulnerabilities()
         {
-            //we get the active plugins
-            $active_plugins = $this->get_active_plugins();
-
-            //now we download the component json file and loop through the active plugins
-            $components = $this->get_plugin_vulnerabilities();
-
-            foreach ($active_plugins as $plugin) {
-                //we get the slug of the plugin
-                $slug = $plugin['TextDomain'];
-                //we get the version of the plugin
-                $version = $plugin['Version'];
-                //we get the vulnerabilities for the plugin
-                $vulnerabilities = $this->get_plugin_vulnerabilities($slug);
-                //we check if there are any vulnerabilities for the plugin
-                if ($vulnerabilities) {
-                    //we check if the plugin is vulnerable
-                    if ($this->is_plugin_vulnerable($vulnerabilities, $version)) {
-                        //we log the error
-                        $this->log_error('Plugin ' . $slug . ' is vulnerable');
-                    }
-                }
+            //we get all the installed plugins
+            $installed_plugins = get_plugins();
+            $vulnerabilities = [];
+            foreach($installed_plugins as $plugin) {
+                $plugin = $plugin['TextDomain'];
+                $url = 'https://api.really-simple-security.com/storage/downloads/plugin/'.$plugin.'.json';
+                $data = $this->download($url);
+                if ($data !== null)
+                $vulnerabilities[] = $data;//json_decode(json_encode($data), true);
             }
-        }
-        private function get_plugin_vulnerabilities()
-        {
-            $url = 'https://wpvulndb.com/api/v3/plugins';
-            return $this->download($url);
 
+            $this->store_file($vulnerabilities);
         }
+
         private function download($url)
         {
             //now we check if the file remotely exists and then log an error if it does not.
@@ -170,86 +127,8 @@ if (!class_exists("rsssl_vulnerabilities")) {
                 //file exists, download it
                 $json = file_get_contents($url);
                 return json_decode($json);
-            } else {
-                //file does not exist, log an error
-                $this->log_error('Could not download vulnerabilities');
-
-                //it could be that there are no vulnerabilities for this version, so we do nothing? TODO:Ask Team what to do with this.
-
-                return null;
             }
-        }
-
-
-
-        /**
-         * Saves the vulnerabilities for the current core version.
-         * TODO: build a check if the version is already in the database, if so, update it.
-         * @param $vulnerabilities
-         * @return void
-         */
-        private function save_core_vulnerabilities($vulnerability)
-        {
-            global $wpdb;
-            global $wp_version;
-            $table_name = $wpdb->prefix . "rsssl_vulnerabilities";
-            $wpdb->query("TRUNCATE TABLE " . $table_name); //TODO: check if this is the best way to do this (performance wise)
-            $wpdb->insert(
-                $table_name,
-                array(
-                    'component' => $vulnerability->name, //this can be the version of the wp core or slug for plugin or theme
-                    'version' => $wp_version, //since we know which core version is running we store it here.
-                    'updated' => $vulnerability->update, //date is fetched from the json file
-                    'severity_critical' => $vulnerability->severity_critical, //severity is fetched from the json file
-                    'severity_high' => $vulnerability->severity_high, //severity is fetched from the json file
-                    'severity_medium' => $vulnerability->severity_medium, //severity is fetched from the json file
-                    'severity_low' => $vulnerability->severity_low, //severity is fetched from the json file
-                    'severity_unknown' => $vulnerability->severity_unknown, //severity is fetched from the json file
-                    'last_checked' => current_time('mysql'),
-                )
-            );
-
-        }
-
-        /**
-         * Checks if the columns are still there, if not, add them.
-         *
-         * @param string $table_name
-         * @return void
-         */
-        private function check_columns(string $table_name): void
-        {
-            global $wpdb;
-
-            //if the table exists we check if the columns are still there
-            $columns = $wpdb->get_col("DESC " . $table_name, 0);
-            if (!in_array("component", $columns)) {
-                $wpdb->query("ALTER TABLE " . $table_name . " ADD component varchar(255) NOT NULL");
-            }
-            if (!in_array("version", $columns)) {
-                $wpdb->query("ALTER TABLE " . $table_name . " ADD version varchar(255) NOT NULL");
-            }
-            if (!in_array("updated", $columns)) {
-                $wpdb->query("ALTER TABLE " . $table_name . " ADD updated datetime DEFAULT '0000-00-00 00:00:00' NOT NULL");
-            }
-            if (!in_array("severity_critical", $columns)) {
-                $wpdb->query("ALTER TABLE " . $table_name . " ADD severity_critical int(5) DEFAULT 0 NOT NULL");
-            }
-            if (!in_array("severity_high", $columns)) {
-                $wpdb->query("ALTER TABLE " . $table_name . " ADD severity_high int(5) DEFAULT 0 NOT NULL");
-            }
-            if (!in_array("severity_medium", $columns)) {
-                $wpdb->query("ALTER TABLE " . $table_name . " ADD severity_medium int(5) DEFAULT 0 NOT NULL");
-            }
-            if (!in_array("severity_low", $columns)) {
-                $wpdb->query("ALTER TABLE " . $table_name . " ADD severity_low int(5) DEFAULT 0 NOT NULL");
-            }
-            if (!in_array("severity_unknown", $columns)) {
-                $wpdb->query("ALTER TABLE " . $table_name . " ADD severity_unknown int(5) DEFAULT 0 NOT NULL");
-            }
-            if (!in_array("last_checked", $columns)) {
-                $wpdb->query("ALTER TABLE " . $table_name . " ADD last_checked datetime DEFAULT '0000-00-00 00:00:00' NOT NULL");
-            }
+            $this->log_error('Could not download file from ' . $url);
         }
 
         private function log_error(string $string)
@@ -257,30 +136,120 @@ if (!class_exists("rsssl_vulnerabilities")) {
             error_log($string);
         }
 
-        private function get_active_plugins()
+        /**
+         * Fetches the active plugins and returns them as an array.
+         *
+         * @return array
+         */
+        private function get_active_plugins(): array
         {
             $active_plugins = get_option('active_plugins');
             $plugins = [];
             foreach ($active_plugins as $plugin) {
                 //we return only the slug and version of the plugin
                 $found = get_plugin_data(WP_PLUGIN_DIR . '/' . $plugin);
-
-
-                $plugins[] = ['slug' => $found['TextDomain'], 'version' => $found['Version']];
+                $plugins[] = $found;
             }
             return $plugins;
         }
 
-        private function enable_wp_cron()
+
+        private function filter_active_components($components, array $active_plugins): array
         {
-            if (!defined('DISABLE_WP_CRON') || DISABLE_WP_CRON) {
-                define('DISABLE_WP_CRON', false);
+            $active_components = [];
+            foreach ($components as $component) foreach ($active_plugins as $active_plugin) if ($component->slug === $active_plugin['slug']) {
+                $component->version = $active_plugin['version'];
+                //now we loop through the vulnerabilities of the component
+                foreach ($component->vulnerabilities as $index => $vulnerability) {
+                    //if the max_version is not set, we remove the vulnerability from the array
+                    if (!isset($vulnerability->max_version) && !isset($vulnerability->min_version)) {
+                        unset($component->vulnerabilities[$index]);
+                    }
+                    //if the max_version is set, we check if it is higher than the version of the plugin
+                    if (!isset($vulnerability->max_version)) {
+                        continue;
+                    }//if the max_version is higher or equal to the version of the plugin, we remove the vulnerability from the array
+                    if (version_compare($active_plugin['version'], $vulnerability->max_version, '>=')) {
+                        unset($component->vulnerabilities[$index]);
+                    }
+                }
+                //now we get all values from the rss-severity property from the vulnerabilities
+
+                $component = $this->count_severities($component);
+                $active_components[] = $component;
             }
+            return $active_components;
         }
 
+        private function count_severities($component)
+        {
+            $severities = wp_list_pluck($component->vulnerabilities, 'rss_severity');
+            // we add all the properties to the component
+            $component->severity_critical = 0;
+            $component->severity_high = 0;
+            $component->severity_medium = 0;
+            $component->severity_low = 0;
+            $component->severity_unknown = 0;
+            //we loop through the severities and add them to the component
+            foreach ($severities as $severity) {
+                switch ($severity) {
+                    case 'c':
+                        $component->severity_critical++;
+                        break;
+                    case 'h':
+                        $component->severity_high++;
+                        break;
+                    case 'm':
+                        $component->severity_medium++;
+                        break;
+                    case 'l':
+                        $component->severity_low++;
+                        break;
+                    default:
+                        $component->severity_unknown++;
+                        break;
+                }
+            }
+            //we now no longer need the vulnerabilities property, so we remove it
+            unset($component->vulnerabilities);
+            return $component;
+        }
+
+        /**
+         * Stores a full core or component file in the upload folder
+         *
+         * @param $data
+         * @param bool $isCore
+         * @return array
+         */
+        private function store_file($data, bool $isCore = false): ?array
+        {
+            //if the data is empty, we return null
+            if (empty($data)) {
+                return null;
+            }
+            //we get the upload directory
+            $upload_dir = wp_upload_dir();
+            $upload_dir = $upload_dir['basedir'];
+            $upload_dir = $upload_dir . '/rsssl';
+
+            $file = $upload_dir . '/' . ($isCore ? 'core.json' : 'components.json');
+
+            //we check if the directory exists, if not, we create it
+            if (!file_exists($upload_dir)) {
+                mkdir($upload_dir, 0755, true);
+            }
+
+            //we delete the old file if it exists
+            if (file_exists($file)) {
+                wp_delete_file($file);
+            }
+
+            file_put_contents($file, json_encode($data));
+            //we return the data
+            return $data;
+        }
     }
-
-
 }
 
 add_action('admin_init', 'rsssl_vulnerabilities');
