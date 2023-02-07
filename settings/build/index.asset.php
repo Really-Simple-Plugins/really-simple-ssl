581746465fd10e37018ac5038d0b8388077249c1
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-02-07 22:42:36 +0100

    add notice in help button

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '79e38cdac768e0adb645');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'c35fcedf99916191bf8b');
