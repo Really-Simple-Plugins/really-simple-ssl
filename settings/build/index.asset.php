0619bf201c91d97869f352f7d017a39e18fa5ba1
Marcel Santing <marcel@really-simple-plugins.com>
2023-02-14 07:57:10 +0100

    ran npm

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'ef8c578cfc3cea31a023');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '5f0d08b16cae27ce8478');
