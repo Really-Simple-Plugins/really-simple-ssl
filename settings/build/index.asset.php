f0650396fad7e2bf95e06d78d0b67a26e28722ad
mwolters90 <mark@really-simple-ssl.com>
2023-02-24 10:39:47 +0100

    component update & build

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'fb1542bba602f7b4443c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'bc8789e83a2396003e33');
--- settings/src/Settings/PostDropDown.js
+++ settings/src/Settings/PostDropDown.js
@@ -1,69 +1,73 @@
+/**
+ * React component for a post dropdown selector.
+ *
+ * This component renders a dropdown that allows the user to select a post and update an option change_login_url_failure_url in the RSSSL settings.
+ * The dropdown includes a search field that allows the user to filter the available posts by title.
+ *
+ * @return {JSX.Element} The rendered React component.
+ */
+
 import React, { useState, useEffect } from "react";
 import { __ } from '@wordpress/i18n';
 import * as rsssl_api from "../utils/api";
 
 const PostDropdown = () => {
+    // State variables for managing the component's state.
     const [posts, setPosts] = useState([]);
     const [selectedPost, setSelectedPost] = useState("");
-    const [defaultPost, setDefaultPost] = useState({ title: { rendered: "404 (default)" }, id: "404_default" });
     const [searchTerm, setSearchTerm] = useState("");
+    const [changeLoginUrlFailureUrl, setChangeLoginUrlFailureUrl] = useState("");
 
     const baseUrl = rsssl_settings.site_url;
 
+    // Fetch the value of an RSSSL option in the WordPress database when the component mounts.
     useEffect(() => {
         // Fetch the value of the "change_login_url_failure_url" option
         rsssl_api.getFields().then((response) => {
             const changeLoginUrlFailureUrl = response.fields.find(
                 (field) => field.id === "change_login_url_failure_url"
             ).value;
+            setChangeLoginUrlFailureUrl(changeLoginUrlFailureUrl);
             setSelectedPost(changeLoginUrlFailureUrl);
         });
     }, []);
 
+    // Fetch the list of posts from the WordPress database when the component mounts or when the `baseUrl` changes.
     useEffect(() => {
         const endpoint = `${baseUrl}wp/v2/posts?per_page=100`;
         fetch(endpoint)
             .then((response) => response.json())
             .then((data) => {
-                // remove any existing post with id "404_default"
-                const filteredData = data.filter(post => post.id !== "404_default");
-                // add default post to beginning of posts state
-                setPosts([defaultPost, ...filteredData]);
+                // add default post to beginning of posts state so the default always shows as the first option
+                setPosts([{ title: { rendered: "404 (default)" }, id: "404_default" }, ...data]);
             });
     }, [baseUrl]);
 
-
+    // Fetch the data for the selected post from the WordPress database when the component mounts.
     useEffect(() => {
-        // Fetch the value of the "change_login_url_failure_url" option
-        rsssl_api.getFields().then((response) => {
-            const changeLoginUrlFailureUrl = response.fields.find(
-                (field) => field.id === "change_login_url_failure_url"
-            ).value;
-            setSelectedPost(changeLoginUrlFailureUrl);
+        const endpoint = `${baseUrl}wp/v2/posts/${changeLoginUrlFailureUrl}`;
+        fetch(endpoint)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.title) {
+                    setSelectedPost(data.title.rendered);
+                }
+            });
+    }, [changeLoginUrlFailureUrl]);
 
-            // Fetch the post data for the selected post
-            const endpoint = `${baseUrl}wp/v2/posts/${changeLoginUrlFailureUrl}`;
-            fetch(endpoint)
-                .then((response) => response.json())
-                .then((data) => {
-                    if (data.title) {
-                        setSelectedPost(data.title.rendered);
-                    }
-                });
-        });
-    }, []);
-
+    // Filter the posts by title based on the current search term.
     const filteredPosts = searchTerm
         ? posts.filter((post) =>
             post.title.rendered.toLowerCase().includes(searchTerm)
         )
         : posts.filter((post) => {
             if (post.id === "404_default") {
-                return defaultPost;
+                return { title: { rendered: "404 (default)" }, id: "404_default" };
             }
             return post;
         });
 
+    // Handle changes to the search term in the search field.
     const handleSearchTermChange = (event) => {
         const inputValue = event.target.value;
         setSearchTerm(inputValue);
@@ -77,12 +81,13 @@ const PostDropdown = () => {
         }
     };
 
+    // Render the component's UI.
     return (
         <div>
             <label htmlFor="rsssl-filter-post-input">
-                {__("Redirect to this post when someone tries to access /wp-admin or /wp-login.php. The default is a 404 page","really-simple-ssl")}
+                {__("Redirect to this post when someone tries to access /wp-admin or /wp-login.php. The default is a 404 page.","really-simple-ssl")}
             </label>
-            <div style={{ position: "relative" }}>
+            <div style={{ position: "relative", width: "350px" }}>
                 <input
                     type="text"
                     id="post-input"
