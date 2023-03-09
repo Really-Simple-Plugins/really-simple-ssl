89ac92c9efdb2e29d5a21bf7abfbf03578ea5cb3
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-03-09 08:32:31 +0100

    not link without cap

--- class-multisite.php
+++ class-multisite.php
@@ -212,6 +212,10 @@ if (!class_exists('rsssl_multisite')) {
 	     */
 
 	    public function plugin_settings_link(array $links): array {
+		    if ( !rsssl_user_can_manage() ) {
+			    return $links;
+		    }
+
 		    $url = add_query_arg(array('page' => 'really-simple-security'), network_admin_url('settings.php') );
 		    $settings_link = '<a href="' . $url . '">' . __("Settings", "really-simple-ssl") . '</a>';
 		    array_unshift($links, $settings_link);
@@ -274,6 +278,7 @@ if (!class_exists('rsssl_multisite')) {
             if ( !is_multisite() || !rsssl_is_networkwide_active() ) {
 				return;
             }
+
 	        if ( !rsssl_user_can_manage() ) {
 		        return;
 	        }
@@ -347,7 +352,7 @@ if (!class_exists('rsssl_multisite')) {
 				$percentage = 100;
             }
 
-            return intval($percentage);
+            return (int) $percentage;
         }
 
 	    /**
