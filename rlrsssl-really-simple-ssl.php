8052f71e4f8d05e4cc229592f7e572f621e736f0
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-03-09 08:33:59 +0100

    version

--- rlrsssl-really-simple-ssl.php
+++ rlrsssl-really-simple-ssl.php
@@ -3,7 +3,7 @@
  * Plugin Name: Really Simple SSL
  * Plugin URI: https://really-simple-ssl.com
  * Description: Lightweight SSL & Hardening Plugin
- * Version: 6.2.1
+ * Version: 6.2.2
  * Author: Really Simple Plugins
  * Author URI: https://really-simple-plugins.com
  * License: GPL2
@@ -109,7 +109,7 @@ class REALLY_SIMPLE_SSL
         if (!defined('rsssl_file') ){
             define('rsssl_file', __FILE__);
         }
-		define('rsssl_version', '6.2.1');
+		define('rsssl_version', '6.2.2');
 		define('rsssl_le_cron_generation_renewal_check', 20);
 		define('rsssl_le_manual_generation_renewal_check', 15);
 	}
