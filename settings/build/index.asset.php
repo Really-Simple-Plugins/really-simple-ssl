85c2605145a3033c4b10a15df50ca4127d59801b
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-03-21 15:49:32 +0100

    remove logs, new build

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'e4929f0b6bea1edec289');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '048ab77214a6ab298741');
