e2cc934072bec087392ecb4002d1b8cf0dc66c85
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-03-24 11:03:36 +0100

    update on enforce/lm

--- settings/src/Settings/FieldsData.js
+++ settings/src/Settings/FieldsData.js
@@ -145,6 +145,7 @@ const useFields = create(( set, get ) => ({
 
         //if no fields were changed, do nothing.
         if ( saveFields.length>0 ) {
+            console.log("get fields over api");
             rsssl_api.setFields(saveFields).then(( response ) => {
                 progress = response.progress;
                 fields = response.fields;
