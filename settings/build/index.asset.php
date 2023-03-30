338b828782ebfa3c7289bd88fbe0f963520e8e32
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-03-30 13:55:12 +0200

    fix if no ajax response error

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '8d02fa16b05e06b66d68');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '0e4c22c943c55cf2fc0c');
