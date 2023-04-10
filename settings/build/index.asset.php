e55ba2699582aaf65842bb57bd43b3ac2f0ec635
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-04-10 22:44:37 +0200

    enforce wpcli constant on deactivate

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '808d5d43767d6b52c0cc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'a2f050a8663b1a2e2ff1');
