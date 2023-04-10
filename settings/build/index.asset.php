f880f69414d134c07cebb6c04a85f3454cb86069
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-04-10 22:32:11 +0200

    update styling

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '4eb2e5bc815200b00ad1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '808d5d43767d6b52c0cc');
