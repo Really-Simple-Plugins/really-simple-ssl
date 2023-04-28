d6f8a0237696113420709beb3ce788de2f73b570
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-04-28 11:15:36 +0200

    optimizations merged

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'c9e5aca8e05c84d61ad1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '34f13c3a91c9a6d722fb');
