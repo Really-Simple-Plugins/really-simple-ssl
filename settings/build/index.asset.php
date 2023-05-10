767495d2478cde2e3c405d83d1e96dacf1a53232
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-05-10 21:23:52 +0200

    fix width of notices

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '01e4016d1f85d416084e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'e812ba718c76b7611420');
