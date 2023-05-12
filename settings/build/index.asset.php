b2dffb6f23ff383979797cccaf875099e341f4ee
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-05-12 12:03:51 +0200

    multisite network activation

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'e812ba718c76b7611420');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'ae9d976e6f1310b21df5');
