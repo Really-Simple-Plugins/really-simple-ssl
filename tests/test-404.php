4065c6c3581e2195630556bcd52eb830d2bda4d0
mwolters90 <mark@really-simple-ssl.com>
2023-05-22 10:38:37 +0200

    burst not yet

--- tests/test-404.php
+++ tests/test-404.php
@@ -58,7 +58,7 @@ class TestUrls extends WP_UnitTestCase {
 		// Now filter the links to only include links from the following domains:
 		// (scan.)really-simple-ssl.com, complianz.io, burst-statistics.com, really-simple-plugins.com, ziprecipes.net and wordpress.org
 		$urls = array_filter( $urls, static function ( $url ) {
-			return preg_match( '/^https?:\/\/(?:really-simple-ssl\.com|scan\.really-simple-ssl\.com|complianz\.io|burst-statistics\.com|really-simple-plugins\.com|ziprecipes\.net|wordpress\.org)/', $url );
+			return preg_match( '/^https?:\/\/(?:really-simple-ssl\.com|scan\.really-simple-ssl\.com|complianz\.io|really-simple-plugins\.com|ziprecipes\.net|wordpress\.org)/', $url );
 		} );
 
 		// Loop through each URL and make an HTTP request
