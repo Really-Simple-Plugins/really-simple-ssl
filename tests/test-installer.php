c0ea5377d678864adda9b93ecdb8814e1d44f32a
Marcel Santing <marcel@really-simple-plugins.com>
2023-06-05 13:00:24 +0200

    cleaned up some code for testing

--- tests/test-installer.php
+++ tests/test-installer.php
@@ -44,30 +44,21 @@ class RssslInstallerTest extends WP_UnitTestCase {
 	}
 
 	public function test_plugin_installation() {
+        $plugins = [
+            'burst-statistics',
+            'complianz-gdpr',
+            'complianz-terms-conditions',
+        ];
 
-		$burst_installer           = new rsssl_installer( 'burst-statistics' );
-		$complianz_gdpr_installer  = new rsssl_installer( 'complianz-gdpr' );
-		$complianz_terms_installer = new rsssl_installer( 'complianz-terms-conditions' );
-
-		$this->assertTrue( $burst_installer->download_plugin(), 'Download of burst-statistics plugin failed.' );
-		// Get clean after every download, otherwise issues with ob_level going up
-		ob_get_clean();
-		$this->assertTrue( $complianz_gdpr_installer->download_plugin(), 'Download of complianz-gdpr plugin failed.' );
-		ob_get_clean();
-		$this->assertTrue( $complianz_terms_installer->download_plugin(), 'Download of complianz-terms-conditions plugin failed.' );
-		ob_get_clean();
-
-		$this->assertTrue( $burst_installer->plugin_is_downloaded(), 'burst-statistics plugin is not downloaded.' );
-		$this->assertTrue( $complianz_gdpr_installer->plugin_is_downloaded(), 'complianz-gdpr plugin is not downloaded.' );
-		$this->assertTrue( $complianz_terms_installer->plugin_is_downloaded(), 'complianz-terms-conditions plugin is not downloaded.' );
-
-		$this->assertTrue( $burst_installer->activate_plugin(), 'Activation of burst-statistics plugin failed.' );
-		$this->assertTrue( $complianz_gdpr_installer->activate_plugin(), 'Activation of complianz-gdpr plugin failed.' );
-		$this->assertTrue( $complianz_terms_installer->activate_plugin(), 'Activation of complianz-terms-conditions plugin failed.' );
-
-		$this->assertTrue( $burst_installer->plugin_is_activated(), 'burst-statistics plugin is not activated.' );
-		$this->assertTrue( $complianz_gdpr_installer->plugin_is_activated(), 'complianz-gdpr plugin is not activated.' );
-		$this->assertTrue( $complianz_terms_installer->plugin_is_activated(), 'complianz-terms-conditions plugin is not activated.' );
-
+        foreach ( $plugins as $plugin ) {
+            ob_start();
+            $installer = new rsssl_installer( $plugin );
+            $this->assertTrue( $installer->download_plugin(), 'Download of ' . $plugin . ' plugin failed.' );
+            ob_get_clean();
+            $this->assertTrue( $installer->plugin_is_downloaded(), $plugin . ' plugin is not downloaded.' );
+            $this->assertTrue( $installer->activate_plugin(), 'Activation of ' . $plugin . ' plugin failed.' );
+            $this->assertTrue( $installer->plugin_is_activated(), $plugin . ' plugin is not activated.' );
+            ob_get_clean();
+        }
 	}
 }
