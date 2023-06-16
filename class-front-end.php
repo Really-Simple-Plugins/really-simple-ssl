5cbf5bb039c1f63c28b37ea772b2548325ef9593
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-06-16 20:01:19 +0200

    restore previous redirect method.

--- class-front-end.php
+++ class-front-end.php
@@ -18,48 +18,12 @@ if ( ! class_exists( 'rsssl_front_end' ) ) {
 	        $this->ssl_enabled = rsssl_get_option('ssl_enabled');
 	        $this->wp_redirect = rsssl_get_option('redirect', 'redirect') === 'wp_redirect';
 			add_action( 'rest_api_init', array($this, 'wp_rest_api_force_ssl'), ~PHP_INT_MAX);
-	        //add_filter( 'wp_safe_redirect_fallback', array($this, 'set_fallback_url'), 10, 2 );
-	        add_filter( 'allowed_redirect_hosts', array($this, 'add_alternative_domain'), 10, 2 );
         }
 
         static function this()
         {
             return self::$_this;
         }
-
-	    /**
-	     * Set fallback url to site url.
-	     * @param string $url
-	     * @param $status
-	     *
-	     * @return string|null
-	     */
-	    public function set_fallback_url( $url, $status ) {
-		    return site_url();
-	    }
-
-	    /**
-	     * If a site has a non www domain, the www domain is not in the allowed_hosts list. As a consenquence, redirects from
-	     * http://www to https://www will redirect to the fallback url. This filter adds the www domain to the allowed hosts list in that case.
-	     * If the site_url is www, add non www. Otherwise add www to the safe list.
-	     * Because a domain can be a subdomain, this will add www.sub.domain.com to the safe list.
-	     * As there is no solid way to check if a domain is a subdomain, this is the best we can do.
-	     *
-	     * @param array $domains
-	     *
-	     * @return array
-	     */
-		public function add_alternative_domain( array $domains): array {
-			$domain = site_url();
-			//Parse to strip off any /subfolder/
-			$parse = parse_url($domain);
-			$domain = $parse['host'] ?? str_replace( array( 'http://', 'https://' ), '', $domain );
-			$new_domain = strpos($domain, 'www.') !== false ? str_replace('www.', '', $domain) : 'www.' . $domain;
-			if (! in_array( $new_domain, $domains, true ) ) {
-				$domains[] = $new_domain;
-			}
-			return $domains;
-		}
 
         /**
          * PHP redirect, when ssl is true.
@@ -95,7 +59,7 @@ if ( ! class_exists( 'rsssl_front_end' ) ) {
 
             if ($this->ssl_enabled && !is_ssl() && !(defined("rsssl_no_rest_api_redirect") && rsssl_no_rest_api_redirect)) {
                 $redirect_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-	            wp_safe_redirect($redirect_url, 301, 'Really Simple SSL');
+				wp_redirect($redirect_url);
                 exit;
             }
         }
