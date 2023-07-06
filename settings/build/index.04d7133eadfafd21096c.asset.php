6a6cf06cf9c66d4efb1f8e49cec11c9db4795dd6
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-07-06 09:46:57 +0200

    encode uri for cloudflare

---
+++ settings/build/index.04d7133eadfafd21096c.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '04d7133eadfafd21096c');
