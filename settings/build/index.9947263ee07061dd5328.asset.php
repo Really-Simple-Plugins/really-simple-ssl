fd5c9dff0285d010758da882d75430a842e53366
Mark <mark@really-simple-ssl.com>
2023-07-19 08:05:14 +0200

    warnings in remaining

---
+++ settings/build/index.9947263ee07061dd5328.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '9947263ee07061dd5328');
