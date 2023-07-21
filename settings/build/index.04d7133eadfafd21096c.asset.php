fae1092544ee674fac7b434610193cee2827bc0d
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-07-21 09:58:42 +0200

    build

--- settings/build/index.04d7133eadfafd21096c.asset.php
+++ settings/build/index.04d7133eadfafd21096c.asset.php
@@ -1,5 +1,1 @@
-<<<<<<<< HEAD:settings/build/index.04d7133eadfafd21096c.asset.php
 <?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '04d7133eadfafd21096c');
-========
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '6371e623232dd380d680');
->>>>>>>> master:settings/build/index.61c074887a5f7c79ad9a.asset.php
