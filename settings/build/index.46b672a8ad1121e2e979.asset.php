059ad9bd73ba7935dbf3f8e1f0a64edff39c9567
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-07-21 10:54:03 +0200

    fix confirm dialog

--- settings/build/index.46b672a8ad1121e2e979.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '46b672a8ad1121e2e979');
---
+++ settings/build/index.9c2aab889a12b983a617.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '9c2aab889a12b983a617');
