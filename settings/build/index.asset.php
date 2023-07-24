1a7a5bd4f13d9b41b1b83414ab3ab3282d795022
Marcel Santing <marcel@really-simple-plugins.com>
2023-07-24 13:23:59 +0200

    brough free in line with pro

--- settings/build/index.asset.php
+++ settings/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'a242ea7760ac1aa8c8d9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '8410e86aa2256b6f29be');
