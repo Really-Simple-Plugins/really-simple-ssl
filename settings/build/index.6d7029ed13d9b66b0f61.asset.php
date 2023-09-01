47620c70af2638beb9c6a8ed2c9338aa7ee4aec1
Mark <mark@really-simple-ssl.com>
2023-09-01 11:47:48 +0200

    build

--- settings/build/index.6d7029ed13d9b66b0f61.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '6d7029ed13d9b66b0f61');
---
+++ settings/build/index.db8d7e2f1bac6b2599c7.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'db8d7e2f1bac6b2599c7');
