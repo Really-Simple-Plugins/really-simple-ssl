f5fe5c8216170c287d96630413b97e231ecfda14
Hessel de Jong <info@hesseldejong.com>
2023-09-07 14:01:29 +0200

    Build

---
+++ settings/build/index.18d6d058a16ca172350f.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '18d6d058a16ca172350f');
