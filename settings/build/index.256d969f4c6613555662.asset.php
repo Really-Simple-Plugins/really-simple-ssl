addfb9ed23518f981ae4bb4c97532be129f0edf6
Mark <mark@really-simple-ssl.com>
2023-09-08 13:51:49 +0200

    build

--- settings/build/index.256d969f4c6613555662.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '85d834195ad4a7300b44');
