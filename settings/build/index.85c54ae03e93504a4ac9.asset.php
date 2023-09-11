5b94c8f0b5b3588f1517e1941c93453c2f0fb85d
Hessel de Jong <info@hesseldejong.com>
2023-09-11 10:52:04 +0200

    Toast + save and continue added

--- settings/build/index.85c54ae03e93504a4ac9.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '85c54ae03e93504a4ac9');
