4b07deabc3973fca5b633a96999b474aaa30f63f
Marcel Santing <marcel@really-simple-plugins.com>
2023-09-14 16:03:43 +0200

    fixed the sorting bug

--- settings/build/index.627d11fff53e9d4ef6a7.js
+++ settings/build/index.627d11fff53e9d4ef6a7.js
@@ -4746,10 +4746,14 @@ var vanilla = (createState) => {
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames based on template
 <<<<<<<< HEAD:settings/build/index.627d11fff53e9d4ef6a7.js
+<<<<<<<< HEAD:settings/build/index.627d11fff53e9d4ef6a7.js
 /******/ 			return "" + chunkId + "." + {"vendors-node_modules_mui_material_Tooltip_Tooltip_js":"0cc86ac6c861846722b1","src_Settings_Settings_js":"902ea4078780eff558ca","src_Menu_Menu_js":"c275f909410871ede758","src_Dashboard_DashboardPage_js":"cc315f71743912c1ce7b","src_Onboarding_OnboardingModal_js":"85fbed60f8e666a7e677","src_Modal_Modal_js":"e34b60e44e1022d3fe24","vendors-node_modules_material-ui_core_esm_TextField_TextField_js-node_modules_react-data-tabl-8e8716":"64aaccd4757f22c45708","src_Settings_Field_js":"2e8d0bb433e02bbd984f","vendors-node_modules_material-ui_lab_esm_Autocomplete_index_js":"515dd4c5b9e6e345a1ea","vendors-node_modules_material-ui_core_esm_styles_index_js":"b2604edf5f43bcfce41a"}[chunkId] + ".js";
 ========
 /******/ 			return "" + chunkId + "." + {"vendors-node_modules_mui_material_Tooltip_Tooltip_js":"0cc86ac6c861846722b1","src_Settings_Settings_js":"b2e4e831a6e4dfefbb5c","src_Menu_Menu_js":"c275f909410871ede758","src_Dashboard_DashboardPage_js":"4f7f9660ad30371d9445","src_Onboarding_OnboardingModal_js":"85fbed60f8e666a7e677","src_Modal_Modal_js":"e34b60e44e1022d3fe24","vendors-node_modules_material-ui_core_esm_TextField_TextField_js-node_modules_react-toastify_-1fccac":"140655a5d7db93a2497a","src_Settings_Field_js":"c881cc9d8901c328bd91","vendors-node_modules_material-ui_lab_esm_Autocomplete_index_js":"515dd4c5b9e6e345a1ea","vendors-node_modules_material-ui_core_esm_styles_index_js":"b2604edf5f43bcfce41a"}[chunkId] + ".js";
 >>>>>>>> 070f831e1 (fixed reset function, however could not resolve warning React issue??):settings/build/index.aa620953ab532f245d16.js
+========
+/******/ 			return "" + chunkId + "." + {"vendors-node_modules_mui_material_Tooltip_Tooltip_js":"0cc86ac6c861846722b1","src_Settings_Settings_js":"b2e4e831a6e4dfefbb5c","src_Menu_Menu_js":"66458ee78e9eb4343d4a","src_Dashboard_DashboardPage_js":"4f7f9660ad30371d9445","src_Onboarding_OnboardingModal_js":"85fbed60f8e666a7e677","src_Modal_Modal_js":"e34b60e44e1022d3fe24","vendors-node_modules_material-ui_core_esm_TextField_TextField_js-node_modules_react-toastify_-1fccac":"140655a5d7db93a2497a","src_Settings_Field_js":"369ae94bbfc5ec881341","vendors-node_modules_material-ui_lab_esm_Autocomplete_index_js":"515dd4c5b9e6e345a1ea","vendors-node_modules_material-ui_core_esm_styles_index_js":"b2604edf5f43bcfce41a"}[chunkId] + ".js";
+>>>>>>>> 79d2a7197 (fixed the sorting bug):settings/build/index.a000b00bee3981302fab.js
 /******/ 		};
 /******/ 	})();
 /******/ 	
@@ -5001,7 +5005,11 @@ document.addEventListener('click', e => {
 /******/ })()
 ;
 <<<<<<<< HEAD:settings/build/index.627d11fff53e9d4ef6a7.js
+<<<<<<<< HEAD:settings/build/index.627d11fff53e9d4ef6a7.js
 //# sourceMappingURL=index.627d11fff53e9d4ef6a7.js.map
 ========
 //# sourceMappingURL=index.aa620953ab532f245d16.js.map
 >>>>>>>> 070f831e1 (fixed reset function, however could not resolve warning React issue??):settings/build/index.aa620953ab532f245d16.js
+========
+//# sourceMappingURL=index.a000b00bee3981302fab.js.map
+>>>>>>>> 79d2a7197 (fixed the sorting bug):settings/build/index.a000b00bee3981302fab.js
--- settings/config/config.php
+++ settings/config/config.php
@@ -837,59 +837,65 @@ function rsssl_fields( $load_values = true ) {
                 ]
             ],
         ],
-        [
-            'id'               => 'limit_login_attempts_users_view',
-            'menu_id'          => 'limit_login_attempts',
-            'group_id'         => 'limit_login_attempts_users',
-            'type'             => 'user-datatable',
-            'action'           => 'user_list',
-            'options'          => [
-                'blocked' => __('Blocked', 'really-simple-ssl'),
-                'locked'  => __('Locked-out', 'really-simple-ssl'),
-                'trusted' => __('Trusted', 'really-simple-ssl'),
-            ],
-            'disabled'         => false,
-            'default'          => false,
-            'react_conditions' => [
-                'relation' => 'AND',
-                [
-                    'enable_limited_login_attempts' => true,
-                ]
-            ],
-            'columns'          => [
-                [
-                    'name'       => __('Username', 'really-simple-ssl'),
-                    'sortable'   => true,
-                    'searchable' => true,
-                    'column'     => 'attempt_value',
-                    'width'      => '20%',
-                ],
-                [
-                    'name'     => __('Status', 'really-simple-ssl'),
-                    'sortable' => false,
-                    'column'   => 'status',
-                    'width'    => '10%',
-                ],
-                [
-                    'name'     => __('Date', 'really-simple-ssl'),
-                    'sortable' => true,
-                    'column'   => 'datetime',
-                ],
-                [
-                    'name'     => '',
-                    'sortable' => false,
-                    'column'   => 'action',
-                    'width'    => '70%',
-                ],
-                ['width' => '1px'],
-
-            ],
-        ],
 //        [
-//            'id'               => 'limit_login_attempts_open_source',
+//            'id'               => 'limit_login_attempts_users_view',
+//            'menu_id'          => 'limit_login_attempts',
+//            'group_id'         => 'limit_login_attempts_users',
+//            'type'             => 'user-datatable',
+//            'action'           => 'user_list',
+//            'options'          => [
+//                'blocked' => __('Blocked', 'really-simple-ssl'),
+//                'locked'  => __('Locked-out', 'really-simple-ssl'),
+//                'trusted' => __('Trusted', 'really-simple-ssl'),
+//            ],
+//            'disabled'         => false,
+//            'default'          => false,
+//            'react_conditions' => [
+//                'relation' => 'AND',
+//                [
+//                    'enable_limited_login_attempts' => true,
+//                ]
+//            ],
+//            'columns'          => [
+//                [
+//                    'name'       => __('Username', 'really-simple-ssl'),
+//                    'sortable'   => true,
+//                    'searchable' => true,
+//                    'column'     => 'attempt_value',
+//                    'width'      => '20%',
+//                ],
+//                [
+//                    'name'     => __('Status', 'really-simple-ssl'),
+//                    'sortable' => false,
+//                    'column'   => 'status',
+//                    'width'    => '10%',
+//                ],
+//                [
+//                    'name'     => __('Date', 'really-simple-ssl'),
+//                    'sortable' => true,
+//                    'column'   => 'datetime',
+//                ],
+//                [
+//                    'name'     => '',
+//                    'sortable' => false,
+//                    'column'   => 'action',
+//                    'width'    => '70%',
+//                ],
+//                ['width' => '1px'],
+//
+//            ],
+//        ],
+//        [
+//            'id'               => 'limit_login_attempts_ip_view',
 //            'menu_id'          => 'limit_login_attempts',
 //            'group_id'         => 'limit_login_attempts_ip_address',
-//            'type'             => 'checkbox',
+//            'type'             => 'ip-address-datatable',
+//            'action'           => 'ip_list',
+//            'options'          => [
+//                'blocked' => __('Blocked', 'really-simple-ssl'),
+//                'locked'  => __('Locked-out', 'really-simple-ssl'),
+//                'trusted' => __('Trusted', 'really-simple-ssl'),
+//            ],
 //            'label'            => __('Enable open source blocklist API etc.', 'really-simple-ssl'),
 //            'disabled'         => false,
 //            'default'          => false,
@@ -899,113 +905,92 @@ function rsssl_fields( $load_values = true ) {
 //                    'enable_limited_login_attempts' => true,
 //                ]
 //            ],
+//            'columns'          => [
+//                [
+//                    'name'       => __('Ip Address', 'really-simple-ssl'),
+//                    'sortable'   => true,
+//                    'searchable' => true,
+//                    'column'     => 'attempt_value',
+//                    'width'      => '20%',
+//                ],
+//                [
+//                    'name'     => __('Status', 'really-simple-ssl'),
+//                    'sortable' => false,
+//                    'column'   => 'status',
+//                    'width'    => '10%',
+//                ],
+//                [
+//                    'name'     => __('Date', 'really-simple-ssl'),
+//                    'sortable' => true,
+//                    'column'   => 'datetime',
+//                ],
+//                [
+//                    'name'     => '',
+//                    'sortable' => false,
+//                    'column'   => 'action',
+//                    'width'    => '70%',
+//                ],
+//                ['width' => '1px'],
+//
+//            ],
 //        ],
-        [
-            'id'               => 'limit_login_attempts_ip_view',
-            'menu_id'          => 'limit_login_attempts',
-            'group_id'         => 'limit_login_attempts_ip_address',
-            'type'             => 'ip-address-datatable',
-            'action'           => 'ip_list',
-            'options'          => [
-                'blocked' => __('Blocked', 'really-simple-ssl'),
-                'locked'  => __('Locked-out', 'really-simple-ssl'),
-                'trusted' => __('Trusted', 'really-simple-ssl'),
-            ],
-            'label'            => __('Enable open source blocklist API etc.', 'really-simple-ssl'),
-            'disabled'         => false,
-            'default'          => false,
-            'react_conditions' => [
-                'relation' => 'AND',
-                [
-                    'enable_limited_login_attempts' => true,
-                ]
-            ],
-            'columns'          => [
-                [
-                    'name'       => __('Ip Address', 'really-simple-ssl'),
-                    'sortable'   => true,
-                    'searchable' => true,
-                    'column'     => 'attempt_value',
-                    'width'      => '20%',
-                ],
-                [
-                    'name'     => __('Status', 'really-simple-ssl'),
-                    'sortable' => false,
-                    'column'   => 'status',
-                    'width'    => '10%',
-                ],
-                [
-                    'name'     => __('Date', 'really-simple-ssl'),
-                    'sortable' => true,
-                    'column'   => 'datetime',
-                ],
-                [
-                    'name'     => '',
-                    'sortable' => false,
-                    'column'   => 'action',
-                    'width'    => '70%',
-                ],
-                ['width' => '1px'],
-
-            ],
-        ],
-        [
-            'id'               => 'limit_login_attempts_country_view',
-            'menu_id'          => 'limit_login_attempts',
-            'group_id'         => 'limit_login_attempts_country',
-            'type'             => 'country-datatable',
-            'action'           => 'country_list',
-            'options'          => [
-                'blocked' => __('Blocked', 'really-simple-ssl'),
-                'locked'  => __('Locked-out', 'really-simple-ssl'),
-                'trusted' => __('Trusted', 'really-simple-ssl'),
-            ],
-            'disabled'         => false,
-            'default'          => false,
-            'react_conditions' => [
-                'relation' => 'AND',
-                [
-                    'enable_limited_login_attempts' => true,
-                ]
-            ],
-            'columns'          => [
-                [
-                    'name'       => '',
-                    'sortable'   => false,
-                    'searchable' => true,
-                    'column'     => 'attempt_value',
-                    'width'      => '2%',
-                ],
-                [
-                    'name'       => __('Country', 'really-simple-ssl'),
-                    'sortable'   => true,
-                    'searchable' => true,
-                    'visible'   => false,
-                    'column'     => 'country_name',
-                    'width'      => '20%',
-                ],
-                [
-                    'name'       => __('Continent', 'really-simple-ssl'),
-                    'sortable'   => true,
-                    'searchable' => true,
-                    'column'     => 'region',
-                    'width'      => '20%',
-                ],
-                [
-                    'name'     => __('Status', 'really-simple-ssl'),
-                    'sortable' => false,
-                    'column'   => 'status',
-                    'width'    => '10%',
-                ],
-                [
-                    'name'     => '',
-                    'sortable' => false,
-                    'column'   => 'action',
-                    'width'    => '70%',
-                ],
-                ['width' => '1px'],
-            ],
-        ],
+//        [
+//            'id'               => 'limit_login_attempts_country_view',
+//            'menu_id'          => 'limit_login_attempts',
+//            'group_id'         => 'limit_login_attempts_country',
+//            'type'             => 'country-datatable',
+//            'action'           => 'country_list',
+//            'options'          => [
+//                'blocked' => __('Blocked', 'really-simple-ssl'),
+//                'locked'  => __('Locked-out', 'really-simple-ssl'),
+//                'trusted' => __('Trusted', 'really-simple-ssl'),
+//            ],
+//            'disabled'         => false,
+//            'default'          => false,
+//            'react_conditions' => [
+//                'relation' => 'AND',
+//                [
+//                    'enable_limited_login_attempts' => true,
+//                ]
+//            ],
+//            'columns'          => [
+//                [
+//                    'name'       => '',
+//                    'sortable'   => false,
+//                    'searchable' => true,
+//                    'column'     => 'attempt_value',
+//                    'width'      => '2%',
+//                ],
+//                [
+//                    'name'       => __('Country', 'really-simple-ssl'),
+//                    'sortable'   => true,
+//                    'searchable' => true,
+//                    'visible'   => false,
+//                    'column'     => 'country_name',
+//                    'width'      => '20%',
+//                ],
+//                [
+//                    'name'       => __('Continent', 'really-simple-ssl'),
+//                    'sortable'   => true,
+//                    'searchable' => true,
+//                    'column'     => 'region',
+//                    'width'      => '20%',
+//                ],
+//                [
+//                    'name'     => __('Status', 'really-simple-ssl'),
+//                    'sortable' => false,
+//                    'column'   => 'status',
+//                    'width'    => '10%',
+//                ],
+//                [
+//                    'name'     => '',
+//                    'sortable' => false,
+//                    'column'   => 'action',
+//                    'width'    => '70%',
+//                ],
+//                ['width' => '1px'],
+//            ],
+//        ],
         [
             'id'               => 'event_log_viewer',
             'menu_id'          => 'limit_login_attempts',
