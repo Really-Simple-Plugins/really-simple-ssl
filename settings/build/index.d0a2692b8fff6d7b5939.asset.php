3004c8a1321cc7f03b94a8109a951aad0fc570f8
Marcel Santing <marcel@really-simple-plugins.com>
2023-09-15 13:36:08 +0200

    fixed a load of issues with ip address datatable

--- settings/build/index.d0a2692b8fff6d7b5939.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '06681e75781c1874746c');
