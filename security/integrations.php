83c741cde3ec3a43515f786e84343cc2fbace8e4
Marcel Santing <marcel@really-simple-plugins.com>
2023-09-18 09:59:15 +0200

    removed some old legacy stuff

--- security/integrations.php
+++ security/integrations.php
@@ -90,6 +90,22 @@ $rsssl_integrations_list = apply_filters( 'rsssl_integrations', array(
         'option_id'            => 'enable_vulnerability_scanner',
 	    'admin_only'           => true,
     ),
+    'limit-login-attempts' => array(
+        'label'                => 'Limit login attempts',
+        'folder'               => 'wordpress',
+        'impact'               => 'medium',
+        'risk'                 => 'medium',
+        'option_id'            => 'limit_login_attempts',
+    ),
+    'two-factor' => array(
+        'label'                => '2FA',
+        'folder'               => 'wordpress/two_fa',
+        'impact'               => 'medium',
+        'risk'                 => 'low',
+        'option_id'            => 'two_fa_enabled',
+        'always_include'       => false,
+    ),
+
 ) );
 
 /**
