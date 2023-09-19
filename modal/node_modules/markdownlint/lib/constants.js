d969cbd5b8e838fb42513d07bcfaf0d7bdfcc5a2
Marcel Santing <marcel@really-simple-plugins.com>
2023-09-19 09:27:36 +0200

    removed unwanted folder

--- modal/node_modules/markdownlint/lib/constants.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-
-"use strict";
-
-module.exports.deprecatedRuleNames = [ "MD002", "MD006" ];
-module.exports.homepage = "https://github.com/DavidAnson/markdownlint";
-module.exports.version = "0.25.1";
