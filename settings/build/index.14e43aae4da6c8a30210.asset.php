845b2bf75376f6c8632947c295a3a63a24bab427
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-09-27 17:43:57 +0200

    obsolete function

---
+++ settings/build/index.14e43aae4da6c8a30210.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '14e43aae4da6c8a30210');
--- settings/src/Menu/MenuItem.js
+++ settings/src/Menu/MenuItem.js
@@ -2,68 +2,23 @@ import { __ } from '@wordpress/i18n';
 import useMenu from "./MenuData";
 
 const MenuItem = (props) => {
-    const {selectedSubMenuItem, selectedMainMenuItem, setSelectedSubMenuItem, subMenu, menu} = useMenu();
-
-    // const {selectedSubMenuItem, selectedMainMenuItem, subMenu, menu} = useMenu();
-
-    /*
-     * Menu is selected if the item is the same, or if it is a child.
-     */
-    let menuIsSelected = selectedSubMenuItem===props.menuItem.id;
-    if (props.menuItem.menu_items) {
-        for (const item of props.menuItem.menu_items){
-            if (item.id === selectedSubMenuItem ){
-                menuIsSelected=true;
-            }
-        }
-    }
+    const {selectedSubMenuItem, selectedMainMenuItem, subMenu, menu} = useMenu();
+    const menuIsSelected = isSelectedMenuItem(selectedSubMenuItem, props.menuItem);
 
     const ensureArray = (data) => {
         return Array.isArray(data) ? data : [data];
-    }
-
-    /**
-     *
-     * @param menuItem
-     * @param event
-     *
-     * Open first submenu item automatically
-     */
-    const handleMainMenuItemClick = (menuItem, event) => {
-        let firstSubMenuItemId = null;
-
-        // Function to find the first submenu item recursively
-        const findFirstSubMenuItem = (item) => {
-            if (item.menu_items && item.menu_items.length > 0) {
-                firstSubMenuItemId = item.menu_items[0].id;
-                if (item.menu_items[0].menu_items) {
-                    findFirstSubMenuItem(item.menu_items[0]);
-                }
-            }
-        };
-
-        findFirstSubMenuItem(menuItem);
-
-        // If a submenu item is found, prevent default and stop propagation
-        if (firstSubMenuItemId) {
-            event.preventDefault();
-            event.stopPropagation();
-            setSelectedSubMenuItem(firstSubMenuItemId);
-        }
     }
 
     let menuClass = menuIsSelected ? ' rsssl-active' : '';
     menuClass += props.menuItem.featured ? ' rsssl-featured' : '';
     menuClass += props.menuItem.new ? ' rsssl-new' : '';
     menuClass += props.menuItem.premium && !rsssl_settings.pro_plugin_active ? ' rsssl-premium' : '';
-    let href = '#'+selectedMainMenuItem+'/'+props.menuItem.id;
-
     let menuLink = props.menuItem.directLink || '#'+selectedMainMenuItem+'/'+props.menuItem.id;
 
     return (
         <>
             {props.menuItem.visible && <div className={"rsssl-menu-item" + menuClass}>
-                <a href={menuLink} onClick={(event) => handleMainMenuItemClick(props.menuItem, event)}>
+                <a href={menuLink} >
                     <span>{props.menuItem.title}</span>
                     {props.menuItem.featured && <><span className='rsssl-menu-item-beta-pill'>{__('Beta', 'really-simple-ssl')}</span></>}
                     {props.menuItem.new && <><span className='rsssl-menu-item-new-pill'>{__('New', 'really-simple-ssl')}</span></>}
@@ -79,3 +34,23 @@ const MenuItem = (props) => {
 }
 
 export default MenuItem
+
+/**
+ * Utility function to check if selected menu item is the current menu item or a child of the current menu item
+ * @param selectedSubMenuItem
+ * @param menuItem
+ * @returns {boolean}
+ */
+const isSelectedMenuItem = (selectedSubMenuItem, menuItem) => {
+    if (selectedSubMenuItem === menuItem.id) {
+        return true;
+    }
+    if (menuItem.menu_items) {
+        for (const item of menuItem.menu_items) {
+            if (item.id === selectedSubMenuItem) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
