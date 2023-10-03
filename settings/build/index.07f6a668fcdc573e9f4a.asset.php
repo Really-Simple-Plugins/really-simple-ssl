b4cf423ab48427bead435a90be303285a14501ba
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-10-03 14:46:36 +0200

    new build

--- settings/build/index.07f6a668fcdc573e9f4a.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '07f6a668fcdc573e9f4a');
---
+++ settings/build/index.5424eb04468f36a0b45e.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '5424eb04468f36a0b45e');
