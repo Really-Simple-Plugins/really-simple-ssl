2d9ddbcc544f1b0b83d9ab9ca97778caeee830e9
Marcel Santing <marcel@really-simple-plugins.com>
2023-10-05 11:19:46 +0200

    ran npm

--- settings/build/index.627d11fff53e9d4ef6a7.asset.php
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:settings/build/index.5424eb04468f36a0b45e.asset.php
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '5424eb04468f36a0b45e');
-========
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '2b7f0661ad885d2deae5');
->>>>>>>> ac3ced474 (fixed client side triggers):settings/build/index.7db23c620da31f60a02f.asset.php
--- settings/build/index.b9dbac97b9bd293ecea4.asset.php
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:settings/build/index.b9dbac97b9bd293ecea4.asset.php
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '6f1a1dd181706f74f965');
-========
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '18d6d058a16ca172350f');
->>>>>>>> 5b94c8f0b (Toast + save and continue added):settings/build/index.18d6d058a16ca172350f.asset.php
