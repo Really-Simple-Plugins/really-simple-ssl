0656e2429e22d4f66d13d85a7b7a80dab70cb58a
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-10-06 17:17:53 +0200

    css changes, build

---
+++ settings/build/index.17964179f4d54ec3a576.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '17964179f4d54ec3a576');
