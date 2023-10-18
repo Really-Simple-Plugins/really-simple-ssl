8ee1c610aed2eb6a56bfd63960c231a8360f2a56
Mark <mark@really-simple-ssl.com>
2023-10-18 12:45:24 +0200

    style, headings

--- settings/build/index.e641b61d26352b3d14e4.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'e641b61d26352b3d14e4');
