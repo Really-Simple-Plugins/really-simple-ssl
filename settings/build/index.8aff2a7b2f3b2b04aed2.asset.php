e8373d4b2406a9807363d78b3ed9e4f8a6b09691
Mark <mark@really-simple-ssl.com>
2023-10-18 09:29:48 +0200

    new yellow label

--- settings/build/index.8aff2a7b2f3b2b04aed2.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '8aff2a7b2f3b2b04aed2');
