c449cdb5fe0d8a972dc328c58d32b75a24098d2a
Mark <mark@really-simple-ssl.com>
2023-11-03 10:41:37 +0100

    comment after

--- settings/src/Settings/Help.js
+++ settings/src/Settings/Help.js
@@ -21,12 +21,7 @@ const Help = (props) => {
                     <summary>{notice.title} <Icon name='chevron-down' /></summary>
                     {/*some notices contain html, like for the htaccess notices. A title is required for those options, otherwise the text becomes the title. */}
                         <div key={1}
-                            // nosemgrep
-                             dangerouslySetInnerHTML={{
-                                 // nosemgrep
-                                 __html:
-                                    // nosemgrep
-                                     DOMPurify.sanitize(notice.text) }}></div>
+                             dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(notice.text) }}></div> {// nosemgrep: react-dangerouslysetinnerhtml}
                     {notice.url && <div key={2} className="rsssl-help-more-info"><a target={target} href={notice.url}>{__("More info", "really-simple-ssl")}</a></div>}
                 </details>
             }
