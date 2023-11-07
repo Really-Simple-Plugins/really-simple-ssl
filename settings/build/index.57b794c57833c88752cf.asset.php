e1228789046ac50e7e3fd87ffe92e70075332666
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-11-07 20:27:38 +0100

    translation

--- settings/build/index.57b794c57833c88752cf.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '57b794c57833c88752cf');
