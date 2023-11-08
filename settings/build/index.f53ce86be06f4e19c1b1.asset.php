60ff9ee32b3890bdb527bc5efcc1c32c8104ab20
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-11-08 11:56:30 +0100

    condition, support button

---
+++ settings/build/index.f53ce86be06f4e19c1b1.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'f53ce86be06f4e19c1b1');
