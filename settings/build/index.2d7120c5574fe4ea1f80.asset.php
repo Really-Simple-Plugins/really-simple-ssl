368a51c8169bb1d81a80ac5f8b11a9f35118783d
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-11-09 12:55:26 +0100

    build

--- settings/build/index.2d7120c5574fe4ea1f80.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '2d7120c5574fe4ea1f80');
