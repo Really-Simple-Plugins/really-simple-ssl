32cc16ba1f39358a6e41438b523f97294e460c08
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-12-06 10:56:15 +0100

    check manually button block highlight

---
+++ settings/build/index.d5f8829ea5280759f831.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '0bc48b7c8f0dc14e8683');
