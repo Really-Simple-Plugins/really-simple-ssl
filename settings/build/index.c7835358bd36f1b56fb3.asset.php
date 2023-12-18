80aba0952fcbd590699197b4d755b355d8694165
Rogier Lankhorst <rogierlankhorst@gmail.com>
2023-12-18 16:22:08 +0100

    css for shield

---
+++ settings/build/index.c7835358bd36f1b56fb3.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'c7835358bd36f1b56fb3');
