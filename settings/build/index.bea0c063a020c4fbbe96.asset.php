ea21c12b39baed22dd2de1b2fb9ca3a3e9e91015
Rogier Lankhorst <rogierlankhorst@gmail.com>
2024-01-02 12:29:38 +0100

    update to master

--- settings/build/index.bea0c063a020c4fbbe96.asset.php
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:settings/build/index.d715eb27158f45087560.asset.php
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '2146d87f9c3c80f79926');
-========
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'bea0c063a020c4fbbe96');
->>>>>>>> master:settings/build/index.bea0c063a020c4fbbe96.asset.php
