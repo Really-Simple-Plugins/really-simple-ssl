79965702afefb4758997f91c93cf046d04c23bcf
Marcel Santing <marcel@really-simple-plugins.com>
2024-01-05 07:53:31 +0100

    created new branch and build js

---
+++ settings/build/index.f1fe9d9ccbc0d87af398.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '7ff010045fbd05752ea2');
