a51213c36d99c3b842e974cd088d6295189d8857
Rogier Lankhorst <rogierlankhorst@gmail.com>
2024-01-17 15:57:09 +0100

    auto complete host

---
+++ settings/build/index.adc9c55cfa2f9cc28d9e.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => 'adc9c55cfa2f9cc28d9e');
