8b12d2137c33435b50e4995e0d67d1428812fcc1
Marcel Santing <marcel@really-simple-plugins.com>
2024-02-20 08:53:36 +0100

    added some docs

--- security/wordpress/vulnerabilities/class-rsssl-folder-name.php
+++ security/wordpress/vulnerabilities/class-rsssl-folder-name.php
@@ -10,6 +10,16 @@ class Rsssl_Folder_Name {
 		$this->verifyAndCreateFolder();
 	}
 
+	/**
+	 * Initializes the folder name for Really Simple SSL.
+	 * If the folder name is already set in the option 'rsssl_folder_name',
+	 * the folder name is set to the value from the option.
+	 * Otherwise, a new folder name is generated using a random unique identifier,
+	 * prefixed with 'really-simple-ssl/', and stored in the option 'rsssl_folder_name'.
+	 * If the folder name is generated, the old files are deleted using Rsssl_File_Storage::DeleteOldFiles().
+	 *
+	 * @return void
+	 */
 	private function initializeFolderName(): void {
 		$rsssl_folder = get_option( 'rsssl_folder_name' );
 
@@ -26,10 +36,24 @@ class Rsssl_Folder_Name {
 		}
 	}
 
-	private function folderName( $name ): string {
+	/**
+	 * Returns the provided name as the folder name for Really Simple SSL.
+	 *
+	 * @param  string  $name  The folder name.
+	 *
+	 * @return string The folder name.
+	 */
+	private function folderName( string $name ): string {
 		return $name;
 	}
 
+	/**
+	 * Verifies the existence of the folder associated with Really Simple SSL.
+	 * If the folder does not exist in the WordPress upload directory,
+	 * the createFolder() method is called to create the folder.
+	 *
+	 * @return void
+	 */
 	private function verifyAndCreateFolder(): void {
 		$upload_dir = wp_upload_dir();
 		if ( ! file_exists( $upload_dir['basedir'] . '/' . $this->folderName ) ) {
@@ -37,6 +61,14 @@ class Rsssl_Folder_Name {
 		}
 	}
 
+	/**
+	 * Creates a folder for Really Simple SSL.
+	 * The folder is created in the uploads directory using the folder name stored in the class property $this->folderName.
+	 * If the folder already exists, no action is taken.
+	 * If the folder cannot be created, an error message is logged to the error log.
+	 *
+	 * @return void
+	 */
 	public function createFolder(): void {
 		$upload_dir  = wp_upload_dir();
 		$folder_path = $upload_dir['basedir'] . '/' . $this->folderName;
