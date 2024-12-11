5f47489066a2c4755a8a52fb595cbf458624e9c1
Rogier Lankhorst <rogierlankhorst@gmail.com>
2024-12-11 10:24:58 +0100

    update to core

--- settings/src/Settings/AutoComplete/AutoCompleteControl.js
+++ settings/src/Settings/AutoComplete/AutoCompleteControl.js
@@ -30,6 +30,11 @@ const AutoCompleteControl = ({field, disabled, value, options, label, onChange }
                 value={ value }
                 id={field.id}
                 options={options}
+                isOptionEqualToValue={(option, value) => {
+                    const optionValue = typeof option.value === "string" ? option.value.toLowerCase() : option.value;
+                    const valueValue = typeof value.value === "string" ? value.value.toLowerCase() : value.value;
+                    return optionValue === valueValue;
+                }}
                 getOptionLabel={(option) => {
                     if ( option && option.label ) {
                         return option.label;
@@ -40,9 +45,6 @@ const AutoCompleteControl = ({field, disabled, value, options, label, onChange }
                     }
                     return option;
                 } }
-                isOptionEqualToValue={(option, value) => {
-                    return option.value === value;
-                }}
                 onChange={(event, newValue) => {
                     let value = newValue && newValue.value ? newValue.value : '';
                     onChange(value);
--- settings/src/Settings/PostDropDown.js
+++ settings/src/Settings/PostDropDown.js
@@ -9,131 +9,67 @@
  */
 
 import React, { useState, useEffect } from "react";
-import { __ } from '@wordpress/i18n';
-import TextField from '@material-ui/core/TextField';
+import { ThemeProvider } from '@mui/material/styles';
 import apiFetch from '@wordpress/api-fetch';
+import { __ } from '@wordpress/i18n';
+import autoCompleteSharedTheme from '../utils/autoCompleteTheme';
+import AutoCompleteControl from "../settings/AutoComplete/AutoCompleteControl";
 import useFields from "./FieldsData";
 
 const PostDropdown = ({ field }) => {
     const [posts, setPosts] = useState([]);
     const [selectedPost, setSelectedPost] = useState("");
-    const {updateField, setChangedField} = useFields();
-    const [ThemeProvider, setThemeProvider] = useState(null);
-    const [theme, setTheme] = useState(null);
-    const [Autocomplete, setAutocomplete] = useState(null);
-    useEffect( () => {
-        import('@material-ui/lab/Autocomplete').then(({ default: Autocomplete }) => {
-            setAutocomplete(() => Autocomplete);
+    const { updateField, setChangedField } = useFields();
+
+    useEffect(() => {
+        // Fetch posts data when the component mounts
+        apiFetch({ path: '/wp/v2/pages?per_page=100' }).then((data) => {
+            const formattedData = data.map(post => ({
+                label: post.title.rendered,
+                value: post.id
+            }));
+            setPosts([{ label: "404 (default)", value: "404_default" }, ...formattedData]);
         });
-
-       import ('@material-ui/core/styles').then(({ createTheme,  ThemeProvider }) => {
-           setThemeProvider(() => ThemeProvider);
-            setTheme(() => createTheme({
-                    typography: {
-                        fontSize: 12,
-                        fontFamily: 'inherit',
-                    },
-                    overrides: {
-                        MuiInputBase: {
-                            root: {
-                                fontSize: '12px',
-                                fontFamily: 'inherit',
-                                height: '40px',
-                            }
-                        },
-                        MuiList: {
-                            root: {
-                                fontSize: '8px',
-                            }
-                        },
-                        MuiAutocomplete: {
-                            inputRoot: {
-                                '& .MuiAutocomplete-input': {
-                                    border: 0,
-                                },
-                                flexWrap: 'inherit',
-                            },
-                            popper: {
-                                fontSize: '12px',
-                            },
-                            paper: {
-                                fontSize: '12px',
-                            },
-                            option: {
-                                fontSize: '12px',
-                            },
-                            root: {
-                                padding: 0,
-                            }
-                        },
-                    },
-                })
-            );
-       });
-
     }, []);
 
-    // Fetch the list of posts from the WordPress database when the component mounts.
     useEffect(() => {
-        apiFetch({ path: '/wp/v2/pages?per_page=100' })
-            .then((data) => {
-                const formattedData = data.map(post => ({
-                    title: post.title.rendered,
-                    id: post.id
-                }));
-                setPosts([{ 'title': "404 (default)", 'id': "404_default" }, ...formattedData]);
-            });
-    }, []);
-
-    // Fetch the data for the selected post from the WordPress database when the component mounts.
-    useEffect(() => {
-        if (field.value !== '404_default') {
+        if (field.value !== "404_default") {
             apiFetch({ path: `wp/v2/pages/${field.value}` })
                 .then((data) => {
                     if (data.title) {
-                        setSelectedPost({ 'title': data.title.rendered, 'id': field.value })
+                        setSelectedPost({ label: data.title.rendered, value: field.value });
                     } else {
-                        setSelectedPost({ 'title': "404 (default)", 'id': '404_default' })
+                        setSelectedPost({ label: "404 (default)", value: "404_default" });
                     }
                 });
         } else {
-            setSelectedPost({ 'title': "404 (default)", 'id': '404_default' })
+            setSelectedPost({ label: "404 (default)", value: "404_default" });
         }
     }, [field.value]);
 
-
-    if (!Autocomplete || !ThemeProvider || !theme) {
-        return null;
-    }
+    const handleChange = (newValue) => {
+        const value = newValue ? newValue : '404_default';
+        updateField(field.id, value);
+        setChangedField(field.id, value);
+    };
 
     return (
-        <div>
-            <label htmlFor="rsssl-filter-post-input">
-                {__("Redirect to this post when someone tries to access /wp-admin or /wp-login.php. The default is a 404 page.","really-simple-ssl")}
-            </label>
-            <ThemeProvider theme={theme}>
-                <Autocomplete
-                    options={posts}
-                    getOptionLabel={(option) => option.title ? option.title : ''}
-                    renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            variant="outlined"
-                            placeholder={__('Search for a post.','really-simple-ssl')}
-                        />
-                    )}
-                    getOptionSelected={(option, value) => {
-                        return option.id === value.id;
-                    }}
-                    onChange={(event, newValue) => {
-                        let value = newValue && newValue.id ? newValue.id : '404_default';
-                        updateField(field.id, value);
-                        setChangedField( field.id, value );
-                    }}
+        <ThemeProvider theme={autoCompleteSharedTheme}>
+            <div>
+                <label htmlFor="rsssl-filter-post-input">
+                    {__("Redirect to this post when someone tries to access /wp-admin or /wp-login.php. The default is a 404 page.", "really-simple-ssl")}
+                </label>
+                <AutoCompleteControl
+                    className="rsssl-select"
+                    field={field}
+                    label={__("Search for a post.", "really-simple-ssl")}
                     value={selectedPost}
+                    options={posts}
+                    onChange={handleChange}
+                    disabled={false}
                 />
-            </ThemeProvider>
-        </div>
+            </div>
+        </ThemeProvider>
     );
 };
 
