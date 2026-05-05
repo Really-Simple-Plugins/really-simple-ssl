8e68d0a37e3932a75f55d6dd3205a7c7d364db73
Mark <mark.wolters@team.blue>
2026-05-05 12:05:09 +0200

    support: release sync

--- settings/build/index.150e63172ef86f413c90.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '150e63172ef86f413c90');
